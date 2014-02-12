2e55270c6153a451ed50986513b743778c30a5b4
David Smith <dsmith@redhat.com>
2014-02-12 14:53:21 -0600

    Added rename()/renameat() testcase and prevent more syscall nesting.
    * testsuite/systemtap.syscall/rename.c: New testcase for rename() and
      renameat().
    * tapset/linux/syscalls2.stp: Prevent syscall nesting in syscall.open,
      and syscall.renameat.
    * tapset/linux/nd_syscalls2.stp: Ditto.
    * tapset/linux/syscalls.stp: Prevent syscall nesting in syscall.mkdirat.
    * tapset/linux/nd_syscalls.stp: Ditto.
    * runtime/linux/compat_unistd.h: Add __NR_compat defines.

--- runtime/linux/compat_unistd.h
+++ runtime/linux/compat_unistd.h
@@ -39,11 +39,20 @@
 #ifndef __NR_ia32_linkat
 #define __NR_ia32_linkat 303
 #endif
+#ifndef __NR_ia32_mkdirat
+#define __NR_ia32_mkdirat 296
+#endif
+#ifndef __NR_ia32_open
+#define __NR_ia32_open 5
+#endif
 #ifndef __NR_ia32_pipe2
 #define __NR_ia32_pipe2 331
 #endif
 #ifndef __NR_ia32_readlinkat
 #define __NR_ia32_readlinkat 305
+#endif
+#ifndef __NR_ia32_renameat
+#define __NR_ia32_renameat 302
 #endif
 #ifndef __NR_ia32_rt_sigprocmask
 #define __NR_ia32_rt_sigprocmask 175
@@ -63,8 +72,11 @@
 #define __NR_compat_getpgid		__NR_ia32_getpgid
 #define __NR_compat_inotify_init1	__NR_ia32_inotify_init1
 #define __NR_compat_linkat		__NR_ia32_linkat
+#define __NR_compat_mkdirat		__NR_ia32_mkdirat
+#define __NR_compat_open		__NR_ia32_open
 #define __NR_compat_pipe2		__NR_ia32_pipe2
 #define __NR_compat_readlinkat		__NR_ia32_readlinkat
+#define __NR_compat_renameat		__NR_ia32_renameat
 #define __NR_compat_rt_sigprocmask	__NR_ia32_rt_sigprocmask
 #define __NR_compat_symlinkat		__NR_ia32_symlinkat
 #define __NR_compat_umount2		__NR_ia32_umount2
@@ -84,8 +96,11 @@
 #define __NR_compat_getpgid		__NR_getpgid
 #define __NR_compat_inotify_init1	__NR_inotify_init1
 #define __NR_compat_linkat		__NR_linkat
+#define __NR_compat_mkdirat		__NR_mkdirat
+#define __NR_compat_open		__NR_open
 #define __NR_compat_pipe2		__NR_pipe2
 #define __NR_compat_readlinkat		__NR_readlinkat
+#define __NR_compat_renameat		__NR_renameat
 #define __NR_compat_rt_sigprocmask	__NR_rt_sigprocmask
 #define __NR_compat_symlinkat		__NR_symlinkat
 #define __NR_compat_umount2		__NR_umount2
