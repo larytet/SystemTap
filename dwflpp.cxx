7db8343aa7b1e6c5d5fd7c25cd95f6b72e5b2931
Jonathan Lebon <jlebon@redhat.com>
2014-02-27 14:43:11 -0500

    dwflpp/prologue: fix DW_AT_producer parsing

--- dwflpp.cxx
+++ dwflpp.cxx
@@ -2081,23 +2081,27 @@ dwflpp::resolve_prologue_endings (func_info_map_t & funcs)
   Dwarf_Attribute producer_attr;
   if (dwarf_attr_integrate(cu, DW_AT_producer, &producer_attr))
     {
-      // GNU C x.x.x YYYYMMDD ...
+      // GNU C[++] x.x.x YYYYMMDD ...
       const char* producer = dwarf_formstring(&producer_attr);
       const char* gnuc = strstr(producer, "GNU C");
       if (gnuc)
         {
-          string version;
-
-          // skip to the version number
-          gnuc += 6;
+          // skip over GNU C[++]
+          gnuc += 4; // skip to C
           const char *space = strchr(gnuc, ' ');
-          if (!space)
-            version.assign(gnuc);
-          else
-            version.assign(gnuc, space-gnuc);
+          if (space && *(space+1) != '\0')
+            {
+              string version;
+              const char* vers = space+1;
+              space = strchr(vers, ' ');
+              if (!space)
+                version.assign(vers);
+              else
+                version.assign(vers, space-vers);
 
-          if (strverscmp(version.c_str(), "4.4.0") < 0)
-            consider_decl_line = true;
+              if (strverscmp(version.c_str(), "4.4.0") < 0)
+                consider_decl_line = true;
+            }
         }
     }
 
