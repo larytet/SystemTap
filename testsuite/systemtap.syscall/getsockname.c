c6ab368a36682b3a1d361eac50ab54225df7c1a8
David Smith <dsmith@redhat.com>
2014-03-20 13:52:30 -0500

    PR16716 partial fix: Better types in 'syscall.{getsockname,getsockopt}'.
    * tapset/linux/syscalls.stp: Fix types in 'syscall.getsockname' and
      'syscall.getsockopt'.
    * tapset/linux/aux_syscalls.stp (_stp_lookup_str): Move to the top of the
      file so that it can be called earlier.
      (_stp_lookup_or_str): Ditto.
      (_sockopt_optname_str): Convert to use _stp_lookup_or_str(). Update list
      of values handled.
      (_sockopt_level_str): Ditto.
    * testsuite/systemtap.syscall/getsockname.c: New test case.
    * testsuite/systemtap.syscall/getsockopt.c: Ditto.

---
+++ testsuite/systemtap.syscall/getsockname.c
@@ -0,0 +1,63 @@
+/* COVERAGE: getsockname */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int main()
+{
+    int sock_stream;
+    int fd_null;
+    struct sockaddr_in sin0, sin1;
+    socklen_t sinlen;
+
+    /* initialize local sockaddr */
+    sin0.sin_family = AF_INET;
+    sin0.sin_port = 0;
+    sin0.sin_addr.s_addr = INADDR_ANY;
+    sinlen = sizeof(sin1);
+
+    sock_stream = socket(PF_INET, SOCK_STREAM, 0);
+    //staptest// socket (PF_INET, SOCK_STREAM, IPPROTO_IP) = NNNN
+
+    bind(sock_stream, (struct sockaddr *)&sin0, sizeof(sin0));
+    //staptest// bind (NNNN, {AF_INET, 0.0.0.0, 0}, 16) = 0
+
+    fd_null = open("/dev/null", O_WRONLY);
+    //staptest// open ("/dev/null", O_WRONLY) = NNNN
+
+    getsockname(-1, (struct sockaddr *)&sin1, &sinlen);
+    //staptest// getsockname (-1, XXXX, XXXX) = -NNNN (EBADF)
+
+    getsockname(fd_null, (struct sockaddr *)&sin1, &sinlen);
+    //staptest// getsockname (NNNN, XXXX, XXXX) = -NNNN (ENOTSOCK)
+
+    getsockname(sock_stream, (struct sockaddr *)-1, &sinlen);
+#ifdef __s390__
+    //staptest// getsockname (NNNN, 0x[7]?[f]+, XXXX) = -NNNN (EFAULT)
+#else
+    //staptest// getsockname (NNNN, 0x[f]+, XXXX) = -NNNN (EFAULT)
+#endif
+
+    getsockname(sock_stream, (struct sockaddr *)&sin1, (socklen_t *)-1);
+#ifdef __s390__
+    //staptest// getsockname (NNNN, XXXX, 0x[7]?[f]+) = -NNNN (EFAULT)
+#else
+    //staptest// getsockname (NNNN, XXXX, 0x[f]+) = -NNNN (EFAULT)
+#endif
+
+    getsockname(sock_stream, (struct sockaddr *)&sin1, (socklen_t *)&sinlen);
+    //staptest// getsockname (NNNN, XXXX, XXXX) = 0
+
+    close(sock_stream);
+    //staptest// close (NNNN) = 0
+
+    close(fd_null);
+    //staptest// close (NNNN) = 0
+
+    return 0;
+}
