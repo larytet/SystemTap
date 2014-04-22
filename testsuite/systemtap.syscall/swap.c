6b3556e3cf185d6e547bb214d3b675ce53b7d284
David Smith <dsmith@redhat.com>
2014-04-22 08:42:59 -0500

    PR16716 partial fix: Better types in 'syscall.{swapon,syslog}'.
    * tapset/linux/syscalls2.stp: Fix types in 'syscall.syslog' and
      'syscall.swapon'. In 'syscall.swapon', decode the flags in the new
      'swapflags' variable.
    * tapset/linux/nd_syscalls2.stp: In 'nd_syscall.swapon', decode the flags
      in the new 'swapflags' variable.
    * tapset/linux/aux_syscalls.stp (_swapon_flags_str): New function.
    * tapset/uconversions.stp (user_string_n2_quoted): If we're in a compat
      task, when printing the pointer value as a number, don't expand it to
      64-bits.
    * testsuite/systemtap.syscall/swap.c: Added more tests.
    * testsuite/systemtap.syscall/syslog.c: New test case.
    * testsuite/buildok/syscalls2-detailed.stp: Added new 'swapflags' variable.
    * testsuite/buildok/nd_syscalls2-detailed.stp: Ditto.

--- testsuite/systemtap.syscall/swap.c
+++ testsuite/systemtap.syscall/swap.c
@@ -5,23 +5,51 @@
 
 int main()
 {
+  /* Actually creating a real swap file here is problematic. It won't
+   * work if we're not root or we're on a filesystem that doesn't
+   * support swap files (like tmpfs or nfs). So, we'll specify a file
+   * that doesn't exist. So, all of the following calls will fail (for
+   * various reasons). */
+
   swapon("foobar_swap", 0);
-  //staptest// swapon ("foobar_swap", 0) =
+  //staptest// swapon ("foobar_swap", 0x0) = -NNNN
 
   swapon("foobar_swap", ((1 << SWAP_FLAG_PRIO_SHIFT) & SWAP_FLAG_PRIO_MASK) | SWAP_FLAG_PREFER);
-  //staptest// swapon ("foobar_swap", 32769) =
+  //staptest// swapon ("foobar_swap", SWAP_FLAG_PREFER|1) = -NNNN
 
   swapon("foobar_swap", ((7 << SWAP_FLAG_PRIO_SHIFT) & SWAP_FLAG_PRIO_MASK) | SWAP_FLAG_PREFER);
-  //staptest// swapon ("foobar_swap", 32775) =
+  //staptest// swapon ("foobar_swap", SWAP_FLAG_PREFER|7) = -NNNN
 
   swapon(0, 0);
-  //staptest// swapon ( *(null), 0) =
+  //staptest// swapon ( *(null), 0x0) =
+
+#ifdef SWAP_FLAG_DISCARD
+  swapon("foobar_swap", SWAP_FLAG_DISCARD);
+  //staptest// swapon ("foobar_swap", SWAP_FLAG_DISCARD) = -NNNN
+#endif
+
+  swapon("foobar_swap", -1);
+  //staptest// swapon ("foobar_swap", SWAP_FLAG_[^ ]+|XXXX|32767) = -NNNN
+
+  swapon((char *)-1, SWAP_FLAG_PREFER);
+#ifdef __s390__
+  //staptest// swapon ([7]?[f]+, SWAP_FLAG_PREFER|0) = -NNNN
+#else
+  //staptest// swapon ([f]+, SWAP_FLAG_PREFER|0) = -NNNN
+#endif
 
   swapoff("foobar_swap");
-  //staptest// swapoff ("foobar_swap") =
+  //staptest// swapoff ("foobar_swap") = -NNNN
 
   swapoff(0);
-  //staptest// swapoff ( *(null)) =
+  //staptest// swapoff ( *(null)) = -NNNN
+
+  swapoff((char *)-1);
+#ifdef __s390__
+  //staptest// swapoff ([7]?[f]+) = -NNNN
+#else
+  //staptest// swapoff ([f]+) = -NNNN
+#endif
 
   return 0;
 }
