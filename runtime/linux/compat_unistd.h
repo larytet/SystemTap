93adcc21339b0f90892ae56a2cb310720d9fc17c
David Smith <dsmith@redhat.com>
2014-04-23 14:37:51 -0500

    PR16716 partial fix: Better types passed to _dfd_str() from syscall probes.
    * tapset/linux/syscalls.stp: Fixed calls to _dfd_str() in
      facessat, fchownat, futimesat, compat_futimesat, and linkat syscall
      probes.
      (syscall.futimesat): Added 'dirfd_str' variable. Fixed nesting.
      (syscall.compat_futimesat): Ditto.
    * tapset/linux/nd_syscalls.stp (nd_syscall.futimesat): Added 'dirfd_str'
      variable. Fixed nesting.
      (nd_syscall.compat_futimesat): Ditto.
    * tapset/linux/syscalls2.stp: Fixed calls to _dfd_str() in openat,
      readlinkat, renameat, symlinkat, unlinkat, and utimensat syscall
      probes.
      (syscall.openat): Added 'dfd' and 'dfd_str' variables.
      (syscall.readlinkat): Added 'dfd_str' variable.
      (syscall.utimensat): Added 'dfd', 'dfd_str', 'filename_uaddr',
      'filename', 'tsp_uaddr', 'flags', and 'flags_str' variables.
      (syscall.compat_utimensat): Ditto.
    * tapset/linux/nd_syscalls2.stp (nd_syscall.openat): Added 'dfd' and
      'dfd_str' variables.
      (nd_syscall.readlinkat): Added 'dfd_str' variable.
      (nd_syscall.utimensat): Added 'dfd', 'dfd_str', 'filename_uaddr',
      'filename', 'tsp_uaddr', 'flags', and 'flags_str' variables.
      (nd_syscall.compat_utimensat): Ditto.
    * tapset/linux/aux_syscalls.stp (_dfd_str): Improve to not ignore
      compatibility mode wrong value (instead, values passed in were fixed).
    * runtime/linux/compat_unistd.h: Add '__NR_compat_futimesat' define.
    * testsuite/systemtap.syscall/alarm.c: Add more tests.
    * testsuite/systemtap.syscall/futimes.c: Ditto.
    * testsuite/buildok/syscalls-detailed.stp: Added basic test for new
      variables.
    * testsuite/buildok/syscalls2-detailed.stp: Ditto.
    * testsuite/buildok/nd_syscalls-detailed.stp: Ditto.
    * testsuite/buildok/nd_syscalls2-detailed.stp: Ditto.

--- runtime/linux/compat_unistd.h
+++ runtime/linux/compat_unistd.h
@@ -33,6 +33,9 @@
 #ifndef __NR_ia32_fchownat
 #define __NR_ia32_fchownat 298
 #endif
+#ifndef __NR_ia32_futimesat
+#define __NR_ia32_futimesat 299
+#endif
 #ifndef __NR_ia32_getpgid
 #define __NR_ia32_getpgid 132
 #endif
@@ -79,6 +82,7 @@
 #define __NR_compat_faccessat		__NR_ia32_faccessat
 #define __NR_compat_fchmodat		__NR_ia32_fchmodat
 #define __NR_compat_fchownat		__NR_ia32_fchownat
+#define __NR_compat_futimesat		__NR_ia32_futimesat
 #define __NR_compat_getpgid		__NR_ia32_getpgid
 #define __NR_compat_inotify_init1	__NR_ia32_inotify_init1
 #define __NR_compat_linkat		__NR_ia32_linkat
@@ -105,6 +109,7 @@
 #define __NR_compat_faccessat		__NR_faccessat
 #define __NR_compat_fchmodat		__NR_fchmodat
 #define __NR_compat_fchownat		__NR_fchownat
+#define __NR_compat_futimesat		__NR_futimesat
 #define __NR_compat_getpgid		__NR_getpgid
 #define __NR_compat_inotify_init1	__NR_inotify_init1
 #define __NR_compat_linkat		__NR_linkat
