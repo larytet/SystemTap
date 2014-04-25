bbb9f3b0cecb88c1709de75be7c47d53e62c6aee
Jonathan Lebon <jlebon@redhat.com>
2014-04-25 15:00:07 -0400

    statement.exp: adapt for RHEL5
    In GCC < 4.4, the decl_line is the line holding the opening brace, not the
    line with the identifier. Since the decl_line is the basis for relative
    line number statement probes, this discrepancy was causing statement.exp
    to have some RELATIVE subtest failures.
    
    Add another empty line so that main@statement.c+4 always falls on an
    empty line, regardless of which is the decl_line.

--- testsuite/systemtap.base/statement.c
+++ testsuite/systemtap.base/statement.c
@@ -9,8 +9,9 @@ int foo(int a)
 void bar(int b) { b += 2; printf("single line! %d\n", b); }
 
 int main(int argc, char** argv)
-{
+{ // NB: for GCC < 4.4, decl_line is at '{', not where the identifier is
    if (argc != 1)
       return 42;
-
+// NB: main@statement.c+4 lands here for GCC >= 4.4
+// NB: main@statement.c+4 lands here for GCC < 4.4
    foo(argv[0][0]); bar(argv[0][1]); return 0; }
