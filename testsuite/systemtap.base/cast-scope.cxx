9267aefb7e7612c04eaf886683b204415e91d772
Jonathan Lebon <jlebon@redhat.com>
2014-05-28 11:24:34 -0400

    BZ1099472: cast-scope.exp: adjust relative lineno

--- testsuite/systemtap.base/cast-scope.cxx
+++ testsuite/systemtap.base/cast-scope.cxx
@@ -8,7 +8,7 @@ length(const std::string& str)
     int res, r;
     STAP_PROBE1(cast-scope, length, &str);
     r = str.length() * 2;
-    STAP_PROBE(cast-scope, dummy); /* Just here to probe line +4. */
+    STAP_PROBE(cast-scope, dummy); /* Just here to probe line +5. */
     res = r / 2;
     STAP_PROBE(cast-scope, dummy2); /* Just here prevent line reordering. */
     return res;
