329dffb483f7d9da38ce5c0821094f42f0d9fb1e
Jonathan Lebon <jlebon@redhat.com>
2014-06-25 14:42:39 -0400

    emit_module_refresh: print newline in error msg

--- translate.cxx
+++ translate.cxx
@@ -1906,7 +1906,7 @@ c_unparser::emit_module_refresh ()
   o->newline() << "if (state != STAP_SESSION_RUNNING && state != STAP_SESSION_STARTING && state != STAP_SESSION_ERROR) {";
   // cannot _stp_warn etc. since we're not in probe context
   o->newline(1) << "#if defined(__KERNEL__)";
-  o->newline() << "printk (KERN_ERR \"stap module notifier triggered in unexpected state %d\", state);";
+  o->newline() << "printk (KERN_ERR \"stap module notifier triggered in unexpected state %d\\n\", state);";
   o->newline() << "#endif";
   o->newline() << "return;";
   o->newline(-1) << "}";
