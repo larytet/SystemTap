larytet/SystemTap

commit 765e69762068aadfbed24cee9e0a2960e68ec55a

translate.cxx: reduce actionremaining after function calls

translate.cxx
@@ -2099,6 +2099,7 @@ struct max_action_info: public functioncall_traversing_visitor
       statement_count = (statement_count > max_statement_count - val) ? max_statement_count : statement_count + val;
     }
   void add_max_stmt_count () { statement_count = max_statement_count; }
+  bool statement_count_finite() { return statement_count < max_statement_count; }
 
   void visit_for_loop (for_loop* stmt) { add_max_stmt_count(); }
   void visit_foreach_loop (foreach_loop* stmt) { add_max_stmt_count(); }
@@ -5048,6 +5049,18 @@ c_unparser::visit_functioncall (functioncall* e)
     o->newline() << "c->locals[c->nesting+1]"
                  << "." << c_funcname (r->name)
                  << ".__retvalue;";
+
+  if (!already_checked_action_count && !session->suppress_time_limits
+      && !session->unoptimized)
+    {
+      max_action_info mai (*session);
+      e->referent->body->visit(&mai);
+      // if an unoptimized function/probe called an optimized function, then
+      // increase the counter, since the subtraction isn't done within an
+      // optimized function
+      if(mai.statement_count_finite())
+        record_actions (mai.statement_count, e->tok, true);
+    }
 }
 
 

commit 0c5ef9de485edf93932fffa41774ad5bdb22ebc2

translator: fix function-call actioncount optimization regression
commit #765e6976 accidentally emitted actioncount-tracking code in the
wrong place in the function-call sequence.

* translate.cxx (visit_functioncall): Move it up a wee little bit.

translate.cxx
@@ -5290,15 +5290,6 @@ c_unparser::visit_functioncall (functioncall* e)
   o->newline() << c_funcname (r->name) << " (c);";
   o->newline() << "if (unlikely(c->last_error)) goto out;";
 
-  // return result from retvalue slot
-  if (r->type == pe_unknown)
-    // If we passed typechecking, then nothing will use this return value
-    o->newline() << "(void) 0;";
-  else
-    o->newline() << "c->locals[c->nesting+1]"
-                 << "." << c_funcname (r->name)
-                 << ".__retvalue;";
-
   if (!already_checked_action_count && !session->suppress_time_limits
       && !session->unoptimized)
     {
@@ -5310,6 +5301,17 @@ c_unparser::visit_functioncall (functioncall* e)
       if(mai.statement_count_finite())
         record_actions (mai.statement_count, e->tok, true);
     }
+
+  // return result from retvalue slot NB: this must be last, for the
+  // enclosing statement-expression ({ ... }) to carry this value.
+  if (r->type == pe_unknown)
+    // If we passed typechecking, then nothing will use this return value
+    o->newline() << "(void) 0;";
+  else
+    o->newline() << "c->locals[c->nesting+1]"
+                 << "." << c_funcname (r->name)
+                 << ".__retvalue;";
+
 }
 
 