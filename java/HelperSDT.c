a799c319d22f8503a437cbd4a5146b52978ca06c
Lukas Berk <lberk@redhat.com>
2014-08-29 15:35:49 -0400

    Drop passing the last parameter to determin_java_type
    *java/HelperSDT.c - drop passing the lsat parameter to determin_java_type
    		    and simply return the casted value instead of assigning
    		    and then returning

--- java/HelperSDT.c
+++ java/HelperSDT.c
@@ -15,7 +15,7 @@ char* get_java_string(JNIEnv *env, jobject _string)
   return string;
 }
 
-int64_t determine_java_type(JNIEnv *env, jobject _arg, int64_t staparg)
+int64_t determine_java_type(JNIEnv *env, jobject _arg)
 {
   jclass class_arg = (*env)->GetObjectClass(env, _arg);
   jfieldID fidNumber = 0;
@@ -27,8 +27,7 @@ int64_t determine_java_type(JNIEnv *env, jobject _arg, int64_t staparg)
     }
   else
     {
-      staparg = (int64_t)(*env)->GetIntField(env, _arg, fidNumber);
-      return staparg;
+      return (int64_t)(*env)->GetIntField(env, _arg, fidNumber);
     }
   fidNumber = (*env)->GetFieldID(env, class_arg, "value", "B");
   if (NULL == fidNumber)
@@ -38,8 +37,7 @@ int64_t determine_java_type(JNIEnv *env, jobject _arg, int64_t staparg)
     }
   else
     {
-      staparg = (int64_t)(*env)->GetByteField(env, _arg, fidNumber);
-      return staparg;
+      return (int64_t)(*env)->GetByteField(env, _arg, fidNumber);
     }
   fidNumber = (*env)->GetFieldID(env, class_arg, "value", "Z");
   if (NULL == fidNumber)
@@ -49,8 +47,7 @@ int64_t determine_java_type(JNIEnv *env, jobject _arg, int64_t staparg)
     }
   else
     {
-      staparg = (int64_t)(*env)->GetBooleanField(env, _arg, fidNumber);
-      return staparg;
+      return (int64_t)(*env)->GetBooleanField(env, _arg, fidNumber);
     }
   fidNumber = (*env)->GetFieldID(env, class_arg, "value", "C");
   if (NULL == fidNumber)
@@ -60,8 +57,7 @@ int64_t determine_java_type(JNIEnv *env, jobject _arg, int64_t staparg)
     }
   else
     {
-      staparg = (int64_t)(*env)->GetCharField(env, _arg, fidNumber);
-      return staparg;
+      return (int64_t)(*env)->GetCharField(env, _arg, fidNumber);
     }
   fidNumber = (*env)->GetFieldID(env, class_arg, "value", "S");
   if (NULL == fidNumber)
@@ -71,8 +67,7 @@ int64_t determine_java_type(JNIEnv *env, jobject _arg, int64_t staparg)
     }
   else
     {
-      staparg = (int64_t)(*env)->GetShortField(env, _arg, fidNumber);
-      return staparg;
+      return (int64_t)(*env)->GetShortField(env, _arg, fidNumber);
     }
   fidNumber = (*env)->GetFieldID(env, class_arg, "value", "J");
   if (NULL == fidNumber)
@@ -82,8 +77,7 @@ int64_t determine_java_type(JNIEnv *env, jobject _arg, int64_t staparg)
     }
   else
     {
-      staparg = (int64_t)(*env)->GetLongField(env, _arg, fidNumber);
-      return staparg;
+      return (int64_t)(*env)->GetLongField(env, _arg, fidNumber);
     }
   fidNumber = (*env)->GetFieldID(env, class_arg, "value", "F");
   if (NULL == fidNumber)
@@ -93,8 +87,7 @@ int64_t determine_java_type(JNIEnv *env, jobject _arg, int64_t staparg)
     }
   else
     {
-      staparg = (int64_t)(*env)->GetFloatField(env, _arg, fidNumber);
-      return staparg;
+      return (int64_t)(*env)->GetFloatField(env, _arg, fidNumber);
     }
   fidNumber = (*env)->GetFieldID(env, class_arg, "value", "D");
   if (NULL == fidNumber)
@@ -104,11 +97,9 @@ int64_t determine_java_type(JNIEnv *env, jobject _arg, int64_t staparg)
     }
   else
     {
-      staparg = (int64_t)(*env)->GetDoubleField(env, _arg, fidNumber);
-      return staparg;
+      return (int64_t)(*env)->GetDoubleField(env, _arg, fidNumber);
     }
-  staparg = (int64_t)(intptr_t)get_java_string(env, _arg);
-  return staparg;
+   return (int64_t)(intptr_t)get_java_string(env, _arg);
 }
 /*
  * Class:     HelperSDT
@@ -131,7 +122,7 @@ JNIEXPORT void JNICALL Java_org_systemtap_byteman_helper_HelperSDT_METHOD_1STAP_
 (JNIEnv *env, jobject obj, jstring _rulename, jobject _arg1)
 {
   char* rulename = get_java_string(env, _rulename);
-  int64_t arg1 = determine_java_type(env, _arg1, arg1);
+  int64_t arg1 = determine_java_type(env, _arg1);
   STAP_PROBE2(HelperSDT, method__1, arg1, rulename);
 }
 
@@ -144,8 +135,8 @@ JNIEXPORT void JNICALL Java_org_systemtap_byteman_helper_HelperSDT_METHOD_1STAP_
 (JNIEnv *env, jobject obj, jstring _rulename, jobject _arg1, jobject _arg2)
 {
   char* rulename = get_java_string(env, _rulename);
-  int64_t arg1 = determine_java_type(env, _arg1, arg1);
-  int64_t arg2 = determine_java_type(env, _arg2, arg2);
+  int64_t arg1 = determine_java_type(env, _arg1);
+  int64_t arg2 = determine_java_type(env, _arg2);
   STAP_PROBE3(HelperSDT, method__2, arg1, arg2, rulename);
 
 }
@@ -159,9 +150,9 @@ JNIEXPORT void JNICALL Java_org_systemtap_byteman_helper_HelperSDT_METHOD_1STAP_
 (JNIEnv *env, jobject obj, jstring _rulename, jobject _arg1, jobject _arg2, jobject _arg3)
 {
   char* rulename = get_java_string(env, _rulename);
-  int64_t arg1 = determine_java_type(env, _arg1, arg1);
-  int64_t arg2 = determine_java_type(env, _arg2, arg2);
-  int64_t arg3 = determine_java_type(env, _arg3, arg3);
+  int64_t arg1 = determine_java_type(env, _arg1);
+  int64_t arg2 = determine_java_type(env, _arg2);
+  int64_t arg3 = determine_java_type(env, _arg3);
   STAP_PROBE4(HelperSDT, method__3, arg1, arg2, arg3, rulename);
 }
 
@@ -174,10 +165,10 @@ JNIEXPORT void JNICALL Java_org_systemtap_byteman_helper_HelperSDT_METHOD_1STAP_
 (JNIEnv *env, jobject obj, jstring _rulename, jobject _arg1, jobject _arg2, jobject _arg3, jobject _arg4)
 {
   char* rulename = get_java_string(env, _rulename);
-  int64_t arg1 = determine_java_type(env, _arg1, arg1);
-  int64_t arg2 = determine_java_type(env, _arg2, arg2);
-  int64_t arg3 = determine_java_type(env, _arg3, arg3);
-  int64_t arg4 = determine_java_type(env, _arg4, arg4);
+  int64_t arg1 = determine_java_type(env, _arg1);
+  int64_t arg2 = determine_java_type(env, _arg2);
+  int64_t arg3 = determine_java_type(env, _arg3);
+  int64_t arg4 = determine_java_type(env, _arg4);
   STAP_PROBE5(HelperSDT, method__4, arg1, arg2, arg3, arg4, rulename);
 }
 
@@ -190,11 +181,11 @@ JNIEXPORT void JNICALL Java_org_systemtap_byteman_helper_HelperSDT_METHOD_1STAP_
 (JNIEnv *env, jobject obj, jstring _rulename, jobject _arg1, jobject _arg2, jobject _arg3, jobject _arg4, jobject _arg5)
 {
   char* rulename = get_java_string(env, _rulename);
-  int64_t arg1 = determine_java_type(env, _arg1, arg1);
-  int64_t arg2 = determine_java_type(env, _arg2, arg2);
-  int64_t arg3 = determine_java_type(env, _arg3, arg3);
-  int64_t arg4 = determine_java_type(env, _arg4, arg4);
-  int64_t arg5 = determine_java_type(env, _arg5, arg5);
+  int64_t arg1 = determine_java_type(env, _arg1);
+  int64_t arg2 = determine_java_type(env, _arg2);
+  int64_t arg3 = determine_java_type(env, _arg3);
+  int64_t arg4 = determine_java_type(env, _arg4);
+  int64_t arg5 = determine_java_type(env, _arg5);
   STAP_PROBE6(HelperSDT, method__5, arg1, arg2, arg3, arg4, arg5, rulename);
 }
 
@@ -207,12 +198,12 @@ JNIEXPORT void JNICALL Java_org_systemtap_byteman_helper_HelperSDT_METHOD_1STAP_
 (JNIEnv *env, jobject obj, jstring _rulename, jobject _arg1, jobject _arg2, jobject _arg3, jobject _arg4, jobject _arg5, jobject _arg6)
 {
   char* rulename = get_java_string(env, _rulename);
-  int64_t arg1 = determine_java_type(env, _arg1, arg1);
-  int64_t arg2 = determine_java_type(env, _arg2, arg2);
-  int64_t arg3 = determine_java_type(env, _arg3, arg3);
-  int64_t arg4 = determine_java_type(env, _arg4, arg4);
-  int64_t arg5 = determine_java_type(env, _arg5, arg5);
-  int64_t arg6 = determine_java_type(env, _arg6, arg6);
+  int64_t arg1 = determine_java_type(env, _arg1);
+  int64_t arg2 = determine_java_type(env, _arg2);
+  int64_t arg3 = determine_java_type(env, _arg3);
+  int64_t arg4 = determine_java_type(env, _arg4);
+  int64_t arg5 = determine_java_type(env, _arg5);
+  int64_t arg6 = determine_java_type(env, _arg6);
   STAP_PROBE7(HelperSDT, method__6, arg1, arg2, arg3, arg4, arg5, arg6, rulename);
 }
 
@@ -225,13 +216,13 @@ JNIEXPORT void JNICALL Java_org_systemtap_byteman_helper_HelperSDT_METHOD_1STAP_
 (JNIEnv *env, jobject obj, jstring _rulename, jobject _arg1, jobject _arg2, jobject _arg3, jobject _arg4, jobject _arg5, jobject _arg6, jobject _arg7)
 {
   char* rulename = get_java_string(env, _rulename);
-  int64_t arg1 = determine_java_type(env, _arg1, arg1);
-  int64_t arg2 = determine_java_type(env, _arg2, arg2);
-  int64_t arg3 = determine_java_type(env, _arg3, arg3);
-  int64_t arg4 = determine_java_type(env, _arg4, arg4);
-  int64_t arg5 = determine_java_type(env, _arg5, arg5);
-  int64_t arg6 = determine_java_type(env, _arg6, arg6);
-  int64_t arg7 = determine_java_type(env, _arg7, arg7);
+  int64_t arg1 = determine_java_type(env, _arg1);
+  int64_t arg2 = determine_java_type(env, _arg2);
+  int64_t arg3 = determine_java_type(env, _arg3);
+  int64_t arg4 = determine_java_type(env, _arg4);
+  int64_t arg5 = determine_java_type(env, _arg5);
+  int64_t arg6 = determine_java_type(env, _arg6);
+  int64_t arg7 = determine_java_type(env, _arg7);
   STAP_PROBE8(HelperSDT, method__7, arg1, arg2, arg3, arg4, arg5, arg6, arg7, rulename);
 }
 
@@ -244,14 +235,14 @@ JNIEXPORT void JNICALL Java_org_systemtap_byteman_helper_HelperSDT_METHOD_1STAP_
 (JNIEnv *env, jobject obj, jstring _rulename, jobject _arg1, jobject _arg2, jobject _arg3, jobject _arg4, jobject _arg5, jobject _arg6, jobject _arg7, jobject _arg8)
 {
   char* rulename = get_java_string(env, _rulename);
-  int64_t arg1 = determine_java_type(env, _arg1, arg1);
-  int64_t arg2 = determine_java_type(env, _arg2, arg2);
-  int64_t arg3 = determine_java_type(env, _arg3, arg3);
-  int64_t arg4 = determine_java_type(env, _arg4, arg4);
-  int64_t arg5 = determine_java_type(env, _arg5, arg5);
-  int64_t arg6 = determine_java_type(env, _arg6, arg6);
-  int64_t arg7 = determine_java_type(env, _arg7, arg7);
-  int64_t arg8 = determine_java_type(env, _arg8, arg8);
+  int64_t arg1 = determine_java_type(env, _arg1);
+  int64_t arg2 = determine_java_type(env, _arg2);
+  int64_t arg3 = determine_java_type(env, _arg3);
+  int64_t arg4 = determine_java_type(env, _arg4);
+  int64_t arg5 = determine_java_type(env, _arg5);
+  int64_t arg6 = determine_java_type(env, _arg6);
+  int64_t arg7 = determine_java_type(env, _arg7);
+  int64_t arg8 = determine_java_type(env, _arg8);
   STAP_PROBE9(HelperSDT, method__8, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, rulename);
 }
 /*
@@ -263,15 +254,15 @@ JNIEXPORT void JNICALL Java_org_systemtap_byteman_helper_HelperSDT_METHOD_1STAP_
 (JNIEnv *env, jobject obj, jstring _rulename, jobject _arg1, jobject _arg2, jobject _arg3, jobject _arg4, jobject _arg5, jobject _arg6, jobject _arg7, jobject _arg8, jobject _arg9)
 {
   char* rulename = get_java_string(env, _rulename);
-  int64_t arg1 = determine_java_type(env, _arg1, arg1);
-  int64_t arg2 = determine_java_type(env, _arg2, arg2);
-  int64_t arg3 = determine_java_type(env, _arg3, arg3);
-  int64_t arg4 = determine_java_type(env, _arg4, arg4);
-  int64_t arg5 = determine_java_type(env, _arg5, arg5);
-  int64_t arg6 = determine_java_type(env, _arg6, arg6);
-  int64_t arg7 = determine_java_type(env, _arg7, arg7);
-  int64_t arg8 = determine_java_type(env, _arg8, arg8);
-  int64_t arg9 = determine_java_type(env, _arg9, arg9);
+  int64_t arg1 = determine_java_type(env, _arg1);
+  int64_t arg2 = determine_java_type(env, _arg2);
+  int64_t arg3 = determine_java_type(env, _arg3);
+  int64_t arg4 = determine_java_type(env, _arg4);
+  int64_t arg5 = determine_java_type(env, _arg5);
+  int64_t arg6 = determine_java_type(env, _arg6);
+  int64_t arg7 = determine_java_type(env, _arg7);
+  int64_t arg8 = determine_java_type(env, _arg8);
+  int64_t arg9 = determine_java_type(env, _arg9);
   STAP_PROBE10(HelperSDT, method__9, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, rulename);
 }
 
@@ -284,16 +275,16 @@ JNIEXPORT void JNICALL Java_org_systemtap_byteman_helper_HelperSDT_METHOD_1STAP_
 (JNIEnv *env, jobject obj, jstring _rulename, jobject _arg1, jobject _arg2, jobject _arg3, jobject _arg4, jobject _arg5, jobject _arg6, jobject _arg7, jobject _arg8, jobject _arg9, jobject _arg10)
 {
   char* rulename = get_java_string(env, _rulename);
-  int64_t arg1 = determine_java_type(env, _arg1, arg1);
-  int64_t arg2 = determine_java_type(env, _arg2, arg2);
-  int64_t arg3 = determine_java_type(env, _arg3, arg3);
-  int64_t arg4 = determine_java_type(env, _arg4, arg4);
-  int64_t arg5 = determine_java_type(env, _arg5, arg5);
-  int64_t arg6 = determine_java_type(env, _arg6, arg6);
-  int64_t arg7 = determine_java_type(env, _arg7, arg7);
-  int64_t arg8 = determine_java_type(env, _arg8, arg8);
-  int64_t arg9 = determine_java_type(env, _arg9, arg9);
-  int64_t arg10 = determine_java_type(env, _arg10, arg10);
+  int64_t arg1 = determine_java_type(env, _arg1);
+  int64_t arg2 = determine_java_type(env, _arg2);
+  int64_t arg3 = determine_java_type(env, _arg3);
+  int64_t arg4 = determine_java_type(env, _arg4);
+  int64_t arg5 = determine_java_type(env, _arg5);
+  int64_t arg6 = determine_java_type(env, _arg6);
+  int64_t arg7 = determine_java_type(env, _arg7);
+  int64_t arg8 = determine_java_type(env, _arg8);
+  int64_t arg9 = determine_java_type(env, _arg9);
+  int64_t arg10 = determine_java_type(env, _arg10);
   STAP_PROBE11(HelperSDT, method__10, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, rulename);
 }
 
