4bce18d8dc7f3b742fd87048b4760eb2d1d9fcee
David Smith <dsmith@redhat.com>
2014-12-04 13:59:11 -0600

    PR16716 partial fix: Fix types in 'syscall.{ioprio_[gs]et,sched_setaffinity}'.
    * tapset/linux/syscalls.stp: Fixed types in syscall.ioprio_get and
      syscall.ioprio_set.
    * tapset/linux/nd_syscalls.stp: Ditto.
    * tapset/linux/syscalls2.stp: Updated syscall.sched_getaffinity and fixed
      types and added 32-bit support to syscall.sched_setaffinity.
    * tapset/linux/nd_syscalls2.stp: Ditto.
    * tapset/linux/aux_syscalls.stp (_mknod_mode_str): Simplified.
      (_stp_ioprio_which_str): New function.
      (_stp_ioprio_value_str): Ditto.
    * testsuite/buildok/aux_syscalls-embedded.stp: Added test for new functions.
    * testsuite/buildok/syscalls-detailed.stp: Tests new convience variables.
    * testsuite/buildok/nd_syscalls-detailed.stp: Ditto.
    * testsuite/systemtap.syscall/mknod.c: Updated.
    * testsuite/systemtap.syscall/sched_getaffinity.c: Updated.
    * testsuite/systemtap.syscall/ioprio.c: New test case.
    * testsuite/systemtap.syscall/sched_setaffinity.c: Ditto.

--- testsuite/systemtap.syscall/mknod.c
+++ testsuite/systemtap.syscall/mknod.c
@@ -11,18 +11,18 @@ int main() {
     // ------- test normal operation
 
     mknod("testfile1", S_IFREG | 0644, 0);
-    //staptest// mknod ("testfile1", S_IFREG|S_IROTH|S_IRGRP|S_IWUSR|S_IRUSR, 0) = 0
+    //staptest// mknod ("testfile1", S_IFREG|0644, 0) = 0
 
     syscall(__NR_mknodat, AT_FDCWD, "testfile2", S_IFREG | 0644, 0);
-    //staptest// mknodat (AT_FDCWD, "testfile2", S_IFREG|S_IROTH|S_IRGRP|S_IWUSR|S_IRUSR, 0) = 0
+    //staptest// mknodat (AT_FDCWD, "testfile2", S_IFREG|0644, 0) = 0
 
     // ------- test nasty things
 
     mknod((const char *)-1, S_IFREG | 0644, 0);
 #ifdef __s390__
-    //staptest// mknod ([7]?[f]+, S_IFREG|S_IROTH|S_IRGRP|S_IWUSR|S_IRUSR, 0) = NNNN
+    //staptest// mknod ([7]?[f]+, S_IFREG|0644, 0) = NNNN
 #else
-    //staptest// mknod ([f]+, S_IFREG|S_IROTH|S_IRGRP|S_IWUSR|S_IRUSR, 0) = NNNN
+    //staptest// mknod ([f]+, S_IFREG|0644, 0) = NNNN
 #endif
 
     mknod("testfile1", -1, 0);
@@ -32,25 +32,25 @@ int main() {
     //staptest// mknod ("testfile1", 01, 4294967295) = NNNN
 
     syscall(__NR_mknodat, (int)-1, "testfile2", S_IFREG | 0644, 0);
-    //staptest// mknodat (-1, "testfile2", S_IFREG|S_IROTH|S_IRGRP|S_IWUSR|S_IRUSR, 0) = NNNN
+    //staptest// mknodat (-1, "testfile2", S_IFREG|0644, 0) = NNNN
 
     syscall(__NR_mknodat, (int)-1, "testfile2", S_IFCHR | 0644, 0);
-    //staptest// mknodat (-1, "testfile2", S_IFCHR|S_IROTH|S_IRGRP|S_IWUSR|S_IRUSR, 0) = NNNN
+    //staptest// mknodat (-1, "testfile2", S_IFCHR|0644, 0) = NNNN
 
     syscall(__NR_mknodat, (int)-1, "testfile2", S_IFBLK | 0644, 0);
-    //staptest// mknodat (-1, "testfile2", S_IFBLK|S_IROTH|S_IRGRP|S_IWUSR|S_IRUSR, 0) = NNNN
+    //staptest// mknodat (-1, "testfile2", S_IFBLK|0644, 0) = NNNN
 
     syscall(__NR_mknodat, (int)-1, "testfile2", S_IFIFO | 0644, 0);
-    //staptest// mknodat (-1, "testfile2", S_IFIFO|S_IROTH|S_IRGRP|S_IWUSR|S_IRUSR, 0) = NNNN
+    //staptest// mknodat (-1, "testfile2", S_IFIFO|0644, 0) = NNNN
 
     syscall(__NR_mknodat, (int)-1, "testfile2", S_IFSOCK | 0644, 0);
-    //staptest// mknodat (-1, "testfile2", S_IFSOCK|S_IROTH|S_IRGRP|S_IWUSR|S_IRUSR, 0) = NNNN
+    //staptest// mknodat (-1, "testfile2", S_IFSOCK|0644, 0) = NNNN
 
     syscall(__NR_mknodat, AT_FDCWD, (const char *)-1, S_IFREG | 0644, 0);
 #ifdef __s390__
-    //staptest// mknodat (AT_FDCWD, [7]?[f]+, S_IFREG|S_IROTH|S_IRGRP|S_IWUSR|S_IRUSR, 0) = NNNN
+    //staptest// mknodat (AT_FDCWD, [7]?[f]+, S_IFREG|0644, 0) = NNNN
 #else
-    //staptest// mknodat (AT_FDCWD, [f]+, S_IFREG|S_IROTH|S_IRGRP|S_IWUSR|S_IRUSR, 0) = NNNN
+    //staptest// mknodat (AT_FDCWD, [f]+, S_IFREG|0644, 0) = NNNN
 #endif
 
     syscall(__NR_mknodat, AT_FDCWD, "testfile2", -1, 0);
--- testsuite/systemtap.syscall/sched_getaffinity.c
+++ testsuite/systemtap.syscall/sched_getaffinity.c
@@ -4,20 +4,24 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sched.h>
+#include <sys/syscall.h>
 
 int main()
 {
     cpu_set_t mask;
 
     sched_getaffinity(0, sizeof(mask), &mask);
-    //staptest// sched_getaffinity (0, XXXX, XXXX) = NNNN
+    //staptest// sched_getaffinity (0, NNNN, XXXX) = NNNN
 
     sched_getaffinity(-1, sizeof(mask), &mask);
-    //staptest// sched_getaffinity (-1, XXXX, XXXX) = -NNNN
+    //staptest// sched_getaffinity (-1, NNNN, XXXX) = -NNNN
 
-    sched_getaffinity(0, (size_t)-1, &mask);
-    // glibc mangles this -1 to 0x7fffffff instead of 0xffffffff
-    //staptest// sched_getaffinity (0, 0x7fffffff, XXXX) = NNNN
+    // glibc mangles this one, so use syscall().
+    //sched_getaffinity(0, (size_t)-1, &mask);
+#ifdef __NR_sched_setaffinity
+    syscall(__NR_sched_getaffinity, (pid_t)0, (size_t)-1, &mask);
+    //staptest// sched_getaffinity (0, 4294967295, XXXX) = NNNN
+#endif
 
     sched_getaffinity(0, sizeof(mask), (cpu_set_t *)-1);
 #ifdef __s390__
