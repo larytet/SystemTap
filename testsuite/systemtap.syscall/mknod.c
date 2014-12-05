c1b1844eb23f3ebfdd562f46467abdd3b88b6625
Martin Cermak <mcermak@redhat.com>
2014-12-05 17:38:50 +0100

    PR16716 partial fix: Fix types in 'syscall.sched_{{get,set}param,get_priority_{max,min},yield}'
    * tapset/linux/aux_syscalls.stp: Fix handling negative args in _sched_policy_str()
    * tapset/linux/nd_syscalls2.stp: Fix argstr in sched_get_priority_{max,min}
    * tapset/linux/syscalls2.stp: Fix types and argstr in sched_get_priority_{max,min}
      and sched_{get,set}param
    * testsuite/systemtap.syscall/mknod.c: Use thin wrapper around the syscall()
    * testsuite/systemtap.syscall/sched.c: New testcase

--- testsuite/systemtap.syscall/mknod.c
+++ testsuite/systemtap.syscall/mknod.c
@@ -6,6 +6,10 @@
 #include <sys/syscall.h>
 #include <linux/fcntl.h>
 
+inline int __mknodat(int dirfd, const char *pathname, mode_t mode, dev_t dev) {
+    return syscall(__NR_mknodat, dirfd, pathname, mode, dev);
+}
+
 int main() {
 
     // ------- test normal operation
@@ -13,7 +17,7 @@ int main() {
     mknod("testfile1", S_IFREG | 0644, 0);
     //staptest// mknod ("testfile1", S_IFREG|0644, 0) = 0
 
-    syscall(__NR_mknodat, AT_FDCWD, "testfile2", S_IFREG | 0644, 0);
+    __mknodat(AT_FDCWD, "testfile2", S_IFREG | 0644, 0);
     //staptest// mknodat (AT_FDCWD, "testfile2", S_IFREG|0644, 0) = 0
 
     // ------- test nasty things
@@ -31,32 +35,32 @@ int main() {
     syscall(__NR_mknod, "testfile1", 1, -1);
     //staptest// mknod ("testfile1", 01, 4294967295) = NNNN
 
-    syscall(__NR_mknodat, (int)-1, "testfile2", S_IFREG | 0644, 0);
+    __mknodat(-1, "testfile2", S_IFREG | 0644, 0);
     //staptest// mknodat (-1, "testfile2", S_IFREG|0644, 0) = NNNN
 
-    syscall(__NR_mknodat, (int)-1, "testfile2", S_IFCHR | 0644, 0);
+    __mknodat(-1, "testfile2", S_IFCHR | 0644, 0);
     //staptest// mknodat (-1, "testfile2", S_IFCHR|0644, 0) = NNNN
 
-    syscall(__NR_mknodat, (int)-1, "testfile2", S_IFBLK | 0644, 0);
+    __mknodat(-1, "testfile2", S_IFBLK | 0644, 0);
     //staptest// mknodat (-1, "testfile2", S_IFBLK|0644, 0) = NNNN
 
-    syscall(__NR_mknodat, (int)-1, "testfile2", S_IFIFO | 0644, 0);
+    __mknodat(-1, "testfile2", S_IFIFO | 0644, 0);
     //staptest// mknodat (-1, "testfile2", S_IFIFO|0644, 0) = NNNN
 
-    syscall(__NR_mknodat, (int)-1, "testfile2", S_IFSOCK | 0644, 0);
+    __mknodat(-1, "testfile2", S_IFSOCK | 0644, 0);
     //staptest// mknodat (-1, "testfile2", S_IFSOCK|0644, 0) = NNNN
 
-    syscall(__NR_mknodat, AT_FDCWD, (const char *)-1, S_IFREG | 0644, 0);
+    __mknodat(AT_FDCWD, (const char *)-1, S_IFREG | 0644, 0);
 #ifdef __s390__
     //staptest// mknodat (AT_FDCWD, [7]?[f]+, S_IFREG|0644, 0) = NNNN
 #else
     //staptest// mknodat (AT_FDCWD, [f]+, S_IFREG|0644, 0) = NNNN
 #endif
 
-    syscall(__NR_mknodat, AT_FDCWD, "testfile2", -1, 0);
+    __mknodat(AT_FDCWD, "testfile2", -1, 0);
     //staptest// mknodat (AT_FDCWD, "testfile2", 037777777777, 0) = NNNN
 
-    syscall(__NR_mknodat, AT_FDCWD, "testfile2", 1, -1);
+    __mknodat(AT_FDCWD, "testfile2", 1, -1);
     //staptest// mknodat (AT_FDCWD, "testfile2", 01, 4294967295) = NNNN
 
     return 0;
