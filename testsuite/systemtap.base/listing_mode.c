2538abbc9414d66095e4e167c4a1733044b389cd
Jonathan Lebon <jlebon@redhat.com>
2014-12-22 12:21:40 -0500

    listing_mode.exp: update for RHEL5
    The listing_mode.exp testcase showed multiple failures on RHEL5. This
    commit fixes these errors by making perf probe testing conditional on
    system support and by adjusting linenos to play nice with RHEL5's GCC.

--- testsuite/systemtap.base/listing_mode.c
+++ testsuite/systemtap.base/listing_mode.c
@@ -8,26 +8,26 @@ int globalvar = 1;
 extern libfoo(int lf);
 
 __attribute__((always_inline))
-inline int inln(int i)
-{
+inline int inln(int i) {
+
    return i % 5;
 }
 
-int bar(int b)
-{
+int bar(int b) {
+
    int i = inln(b);
    printf("inln returned %d\n", i);
    return i * 3;
 }
 
-int foo(int f)
-{
+int foo(int f) {
+
    int b = bar(f);
    return b * 2;
 }
 
-int main(void)
-{
+int main(void) {
+
    globalvar = foo(globalvar);
    globalvar = libfoo(globalvar);
 main_label:
@@ -39,20 +39,20 @@ main_label:
 #elif LISTING_MODE_LIB
 
 __attribute__((always_inline))
-inline int libinln(int i)
-{
+inline int libinln(int i) {
+
    return i % 5;
 }
 
-int libbar(int lb)
-{
+int libbar(int lb) {
+
    int i = libinln(lb);
    printf("inln returned %d\n", i);
    return i * 3;
 }
 
-int libfoo(int lf)
-{
+int libfoo(int lf) {
+
    int lb = libbar(lf);
 lib_label:
    STAP_PROBE1(lib, mark, lb);
