f41a7c4685314294b081996370378c8fb080d1d8
Abegail Jakop <ajakop@redhat.com>
2014-12-31 10:22:11 -0500

    PR12276: tests for symfileline and related
    [u]context-symbols-embedded.stp: check that [u]symfileline() and
    the related tapset functions build without issues
    usymfileline.*: tests usymfileline(), usymfile(), and usymline() with
    and without debug_line info
    symfileline.*: tests symfileline(), symfile() and symline() with
    an valid address from the kernel module set up in context.exp
    context.exp: add symfileline to list of tests to be run

---
+++ testsuite/systemtap.context/symfileline.tcl
@@ -0,0 +1,28 @@
+set correct_fileline 0
+set correct_file 0
+set correct_line 0
+set testname symfileline
+
+# tests functions symfileline, symfile and symline
+
+set expectedoutput {"systemtap_test_module2.c:\[1-9]\[0-9]*" "\[1-9]\[0-9]*" "systemtap_test_module2.c"}
+
+spawn stap -d kernel -d systemtap_test_module2  $srcdir/$subdir/$testname.stp
+expect {
+  -timeout 120
+  "READY" { exec echo 1 > /proc/stap_test_cmd
+    expect {
+      -re {fileline systemtap_test_module2.c:[1-9][0-9]*} { incr correct_fileline; exp_continue }
+      -re {file systemtap_test_module2.c} { incr correct_file; exp_continue }
+      -re {line [1-9][0-9]*} { incr correct_line; exp_continue }
+    }
+  }
+  timeout {fail "$testname (timeout)"}
+}
+
+if { $correct_fileline } { pass "symfileline ()" } { fail "symfileline ()" }
+if { $correct_file } { pass "symfile ()" } { fail "symfile ()" }
+if { $correct_line } { pass "symline ()" } { fail "symline ()" }
+
+catch { close }
+wait
---
+++ testsuite/systemtap.context/usymfileline.c
@@ -0,0 +1,8 @@
+void test_method() {
+  return;
+}
+
+int main(int argc, char **argv) {
+  test_method();
+  return 0;
+}
