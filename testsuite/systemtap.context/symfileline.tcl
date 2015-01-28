93224fc2c0c70186fab5ac81c6e3850cd3ed8d7f
Abegail Jakop <ajakop@redhat.com>
2015-01-28 11:43:46 -0500

    PR12276: compare symfileline() with pp()
    [u]symfileline.*: added a test that uses *.statement(*@*:*) probes to
    compare the return from [u]symfileline() with the @file:line portion of pp()

--- testsuite/systemtap.context/symfileline.tcl
+++ testsuite/systemtap.context/symfileline.tcl
@@ -1,6 +1,7 @@
 set correct_fileline 0
 set correct_file 0
 set correct_line 0
+set pp_matched 0
 set testname symfileline
 
 # tests functions symfileline, symfile and symline
@@ -8,11 +9,12 @@ set testname symfileline
 spawn stap -d kernel -d systemtap_test_module2  $srcdir/$subdir/$testname.stp
 expect {
   -timeout 120
-  "READY" { exec echo 1 > /proc/stap_test_cmd
+  "READY" { exec echo 0 > /proc/stap_test_cmd
     expect {
       -re {fileline .*systemtap_test_module2.c:[1-9][0-9]*} { incr correct_fileline; exp_continue }
       -re {file .*systemtap_test_module2.c} { incr correct_file; exp_continue }
       -re {line [1-9][0-9]*} { incr correct_line; exp_continue }
+      -re {matched} { incr pp_matched; exp_continue }
     }
   }
   timeout {fail "$testname (timeout)"}
@@ -21,6 +23,7 @@ expect {
 if { $correct_fileline } { pass "symfileline ()" } { fail "symfileline ()" }
 if { $correct_file } { pass "symfile ()" } { fail "symfile ()" }
 if { $correct_line } { pass "symline ()" } { fail "symline ()" }
+if { $pp_matched } { pass "symfileline in pp ()" } { fail "symfileline in pp ()" }
 
 catch { close }
 wait
