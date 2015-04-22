c3892b38a6ea02c81ae6350e27405718cb1695a2
Martin Cermak <mcermak@redhat.com>
2015-04-22 08:10:46 +0200

    PR18184: Test coverage improvements, tapset extension.
    * tapset/linux/aux_syscalls.stp: Add auxiliary functions for quotactl tapset.
    * tapset/linux/nd_syscalls2.stp: Fix types for personality tapset,
                                     extend argstr for quotactl.
    * tapset/linux/syscalls2.stp: Ditto.
    * tapset/linux/syscalls.stpm: New macro @__quotactl_argstr().
    * testsuite/systemtap.syscall/personality.c: New testcase.
    * testsuite/systemtap.syscall/pivot_root.c: New testcase.
    * testsuite/systemtap.syscall/quotactl.c: Testcase extension.

---
+++ testsuite/systemtap.syscall/pivot_root.c
@@ -0,0 +1,31 @@
+/* COVERAGE: pivot_root */
+
+#define _GNU_SOURCE
+#include <unistd.h>
+#include <sys/syscall.h>
+
+int main()
+{
+
+    // We are not really going to let the syscall succeed.
+    // Just sanity check it:
+
+    syscall(__NR_pivot_root, "/tmp", "/");
+    //staptest// pivot_root ("/tmp", "/") = NNNN
+
+    syscall(__NR_pivot_root, (const char *)-1, "/tmp");
+#ifdef __s390__
+    //staptest// pivot_root ([7]?[f]+, "/tmp") = NNNN
+#else
+    //staptest// pivot_root ([f]+, "/tmp") = NNNN
+#endif
+
+    syscall(__NR_pivot_root, "/tmp", (const char *)-1);
+#ifdef __s390__
+    //staptest// pivot_root ("/tmp", [7]?[f]+) = NNNN
+#else
+    //staptest// pivot_root ("/tmp", [f]+) = NNNN
+#endif
+
+    return 0;
+}
