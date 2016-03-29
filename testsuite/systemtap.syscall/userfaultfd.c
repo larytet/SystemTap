c7f278dda2e2944ff1c691aea90f8300307efde2
David Smith <dsmith@redhat.com>
2016-03-29 14:53:20 -0500

    Fix PR19876 by adding tapset support for the 'userfaultfd' system call.
    * tapset/linux/syscalls2.stp: Add support for the 'userfaultfd' system
      call.
    * tapset/linux/nd_syscalls.stp: Ditto.
    * tapset/linux/aux_syscalls.stp (_userfaultfd_flags_str): New function.
      (_sys_pipe2_flag_str): Added O_DIRECT to the list of supported flags.
    * testsuite/buildok/syscalls2-detailed.stp: Add userfaultfd test.
    * testsuite/buildok/nd_syscalls2-detailed.stp: Ditto.
    * testsuite/systemtap.syscall/userfaultfd.c: Add new
      syscall.exp/nd_syscall.exp test case.

---
+++ testsuite/systemtap.syscall/userfaultfd.c
@@ -0,0 +1,45 @@
+/* COVERAGE: userfaultfd */
+
+#define _GNU_SOURCE
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/syscall.h>
+
+// If SYS_userfaultfd is defined, let's assume that linux/userfaultfd.h
+// exists.
+#ifdef SYS_userfaultfd
+#include <linux/userfaultfd.h>
+
+static inline int userfaultfd(int flags)
+{
+    return syscall(SYS_userfaultfd, flags);
+}
+#endif
+
+int main()
+{
+#ifdef SYS_userfaultfd
+    int fd;
+    struct uffdio_api api;
+
+    fd = userfaultfd(0);
+    //staptest// userfaultfd (0x0) = NNNN
+
+    // userfaulfd API sanity check
+    memset(&api, 0, sizeof(api));
+    api.api = UFFD_API;
+    api.features = 0;
+    ioctl(fd, UFFDIO_API, &api);
+    //staptest// ioctl (NNNN, NNNN, XXXX) = NNNN
+
+    close(fd);
+    //staptest// close (NNNN) = 0
+
+    fd = userfaultfd(-1);
+    //staptest// userfaultfd (O_[^ ]+|XXXX) = -NNNN
+#endif
+    return 0;
+}
