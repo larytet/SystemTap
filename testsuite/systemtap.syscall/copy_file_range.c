ebfe0d9a4c461031e35c3ae6f5886c55ee0aa0b6
David Smith <dsmith@redhat.com>
2016-03-30 12:23:27 -0500

    Finish fixing PR19882 by updating support for the copy_file_range() syscall.
    * tapset/linux/syscalls.stp: Corrected argstr formatting for
      'copy_file_range'.
    * tapset/linux/nd_syscalls.stp: Corrected argstr formatting and types for
      'copy_file_range'.
    * testsuite/buildok/nd_syscalls-detailed.stp: Updated copy_file_range test.
    * testsuite/buildok/syscalls-detailed.stp: Ditto.
    * testsuite/systemtap.syscall/copy_file_range.c: Got copy_file_range()
      test working.

--- testsuite/systemtap.syscall/copy_file_range.c
+++ testsuite/systemtap.syscall/copy_file_range.c
@@ -1,3 +1,5 @@
+/* COVERAGE: copy_file_range */
+#define _GNU_SOURCE
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,52 +11,73 @@
 #include <sys/types.h>
 #include <limits.h>
 
+#ifdef SYS_copy_file_range
+static loff_t
+copy_file_range(int fd_in, loff_t *off_in, int fd_out,
+		loff_t *off_out, size_t len, unsigned int flags)
+{
+    return syscall(SYS_copy_file_range, fd_in, off_in, fd_out,
+		   off_out, len, flags);
+}
+#endif
+
 int main ()
 {
-  int fd_init, fd_in, fd_out;
-  int off = 0;
-  char buf [20];
-  memset(buf, 9, sizeof(buf));
+#ifdef SYS_copy_file_range
+    int fd_in, fd_out;
+    loff_t off_in, off_out;
+    char buf[] = "Hello world abcdefghijklmnopqrstuvwxyz 01234567890123456789";
+    loff_t buf_size = sizeof(buf) - 1;
 
-	/* create a file with something in it */
-  fd_init = creat("foobar",S_IREAD|S_IWRITE);
-	write(fd_init, buf, sizeof(buf));
-	fsync(fd_init);
-	close(fd_init);
+    /* Create a file with something in it. */
+    fd_in = open("foobar", O_WRONLY|O_CREAT, 0666);
+    write(fd_in, buf, buf_size);
+    fsync(fd_in);
+    close(fd_in);
 
-  fd_in = open("foobar", S_IREAD);
-  fd_out = creat("foobar2",S_IREAD|S_IWRITE|S_IRWXO);
+    fd_in = open("foobar", O_RDONLY);
+    fd_out = open("foobar2", O_WRONLY|O_CREAT, 0666);
 
-  /* Test variations of the copy_file range to make sure that the syscall tapset
-   * prints out the correct values
-   * note: the 22 == EINVAL */
-  copy_file_range(fd_in, &off, fd_out, &off, sizeof(buf), 0);
-  //staptest// copy_file_range (NNNN, XXXX, NNNN, XXXX, 20, NNNN) = -?22
+    /* Copy 'foobar' to 'foobar2'. */
+    off_in = off_out = 0;
+    copy_file_range(fd_in, &off_in, fd_out, &off_out, buf_size, 0);
+    //staptest// copy_file_range (NNNN, XXXX, NNNN, XXXX, 59, 0x0) = 59
 
-  copy_file_range(-1, &off, fd_out, &off, sizeof(buf), 0);
-  //staptest// copy_file_range (-1, XXXX, NNNN, XXXX, 20, NNNN) = NNNN (EBADF)
+    /* Test variations of the copy_file range to make sure that the
+     * syscall tapset prints out the correct values. */
+    copy_file_range(-1, &off_in, fd_out, &off_out, buf_size, 0);
+    //staptest// copy_file_range (-1, XXXX, NNNN, XXXX, 59, 0x0) = -NNNN
 
-  copy_file_range(fd_in, (off_t*)-1, fd_out, &off, sizeof(buf), 0);
-  //staptest// copy_file_range (NNNN, 0x[7]?[f]+, NNNN, XXXX, 20, NNNN)
+    copy_file_range(fd_in, (off_t*)-1, fd_out, &off_out, buf_size, 0);
+#ifdef __s390__
+    //staptest// copy_file_range (NNNN, 0x[7]?[f]+, NNNN, XXXX, 59, 0x0) = -NNNN
+#else
+    //staptest// copy_file_range (NNNN, 0x[f]+, NNNN, XXXX, 59, 0x0) = -NNNN
+#endif
 
-  copy_file_range(fd_in, &off, -1, &off, sizeof(buf), 0);
-  //staptest// copy_file_range (NNNN, XXXX, -1, XXXX, 20, NNNN) = NNNN (EBADF)
+    copy_file_range(fd_in, &off_in, -1, &off_out, buf_size, 0);
+    //staptest// copy_file_range (NNNN, XXXX, -1, XXXX, 59, 0x0) = -NNNN
 
-  copy_file_range(fd_in, &off, fd_out, (off_t*)-1, sizeof(buf), 0);
-  //staptest// copy_file_range (NNNN, XXXX, NNNN, 0x[7]?[f]+, 20, NNNN)
+    copy_file_range(fd_in, &off_in, fd_out, (off_t*)-1, buf_size, 0);
+#ifdef __s390__
+    //staptest// copy_file_range (NNNN, XXXX, NNNN, 0x[7]?[f]+, 59, 0x0) = -NNNN
+#else
+    //staptest// copy_file_range (NNNN, XXXX, NNNN, 0x[f]+, 59, 0x0) = -NNNN
+#endif
 
-  copy_file_range(fd_in, &off, fd_out, &off, 123456789, 0);
-  //staptest// copy_file_range (NNNN, XXXX, NNNN, XXXX, 123456789, NNNN)
+    copy_file_range(fd_in, &off_in, fd_out, &off_out, -1, 0);
+    //staptest// copy_file_range (NNNN, XXXX, NNNN, XXXX, -1, 0x0) = -NNNN
 
-  /* Note: flags is unused and should be set to 0, otherwise an error occurs.
-   * this may change if the syscall is developed */
-  copy_file_range(fd_in, &off, fd_out, &off, sizeof(buf), -1);
-  //staptest// copy_file_range (NNNN, XXXX, NNNN, XXXX, 20, 0) = NNNN (EINVAL)
+    /* Note: flags is unused and should be set to 0, otherwise an
+     * error occurs. This may change if the syscall is developed. */
+    copy_file_range(fd_in, &off_in, fd_out, &off_out, buf_size, -1);
+    //staptest// copy_file_range (NNNN, XXXX, NNNN, XXXX, 59, 0xffffffff) = -NNNN
 
-	close(fd_out);
-	close(fd_in);
-	unlink("foobar");
-	unlink("foobar2");
+    close(fd_out);
+    close(fd_in);
+    unlink("foobar");
+    unlink("foobar2");
+#endif
 
-  return 0;
+    return 0;
 }
