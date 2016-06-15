73ec640306647ff5d021e3fe86ce904692918100
Cody Santing <csanting@redhat.com>
2016-06-15 13:04:06 -0400

    PR11637: Updated probe point for test case

--- testsuite/systemtap.base/set_user.c
+++ testsuite/systemtap.base/set_user.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/mman.h>
+#include <sys/sdt.h>
 int main()
 {
   char cbuf[64];
   
   mlockall(MCL_CURRENT);
   memset(cbuf,'a',sizeof(cbuf));
+  STAP_PROBE(set_user.exe,point);
   exit(0);
 }
