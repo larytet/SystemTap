aa52cbb3bf6e6e3c355b383a686410871a5d435c
Felix Lu <flu@redhat.com>
2016-06-30 14:54:18 -0400

    PR19953: netfilter packet content dumping
    * tapset/linux/conversions.stp: New overloaded instance of
      kernel_buffer_quoted using MAXSTRINGLEN by default.
      Mark kernel_buffer* with /* pure */.
    * tapset/linux/netfilter.stp: New data_str and data_hex variables
      containing buffer contents.
    * translate.cxx: Increase memory dump limit to PAGE_SIZE for memory
      dumping sk_buff page fragments in c_unparser::visit_print.
    * systemtap.examples/network/packet_contents.stp: Example script.
    * systemtap.examples/network/packet_contents.txt: Example demo.

--- translate.cxx
+++ translate.cxx
@@ -5755,7 +5755,7 @@ c_unparser::visit_print_format (print_format* e)
 
 	    /* Limit how much can be printed at a time. (see also PR10490) */
 	    o->newline() << "c->last_stmt = " << lex_cast_qstring(*prec_tok) << ";";
-	    o->newline() << "if (" << mem_size << " > 1024) {";
+	    o->newline() << "if (" << mem_size << " > PAGE_SIZE) {";
 	    o->newline(1) << "snprintf(c->error_buffer, sizeof(c->error_buffer), "
 			  << "\"%lld is too many bytes for a memory dump\", (long long)"
 			  << mem_size << ");";
