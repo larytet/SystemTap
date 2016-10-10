42ca449e388c60b00f8d40dbed806f8dc0c642ec
David Smith <dsmith@redhat.com>
2016-10-10 13:16:40 -0500

    Add python 3 support to python/HelperSDT.
    * python/HelperSDT/_HelperSDT.c: Add python 3 support.
    * python/Makefile.am: Add python 3 support.
    * python/Makefile.in: Regenerated.
    * aclocal.m4: Ditto.
    * configure: Ditto.

--- python/HelperSDT/_HelperSDT.c
+++ python/HelperSDT/_HelperSDT.c
@@ -67,16 +67,66 @@ static PyMethodDef HelperSDT_methods[] = {
 	{NULL, NULL, 0, NULL}        /* Sentinel */
 };
 
-static const char const *HelperSDT_doc =
-    "This module provides an interface for interfacing between Python tracing events and systemtap.";
+PyDoc_STRVAR(HelperSDT_doc,
+	     "This module provides an interface for interfacing between Python tracing events and systemtap.");
+
+#if PY_MAJOR_VERSION >= 3
+//
+// According to <https://docs.python.org/3/c-api/module.html>:
+//
+// ====
+// Module state may be kept in a per-module memory area that can be
+// retrieved with PyModule_GetState(), rather than in static
+// globals. This makes modules safe for use in multiple
+// sub-interpreters.
+//
+// This memory area is allocated based on m_size on module creation,
+// and freed when the module object is deallocated, after the m_free
+// function has been called, if present.
+//
+// Setting m_size to -1 means that the module does not support
+// sub-interpreters, because it has global state.
+//
+// Setting it to a non-negative value means that the module can be
+// re-initialized and specifies the additional amount of memory it
+// requires for its state. Non-negative m_size is required for
+// multi-phase initialization.
+// ====
+//
+// This C module has no module state, so we'll set m_size to -1 (and
+// m_slots, m_traverse, m_clear, and m_free to NULL).
+//
+// All state information is held by the python HelperSDT module, not
+// this _HelperSDT helper C extension module.
+
+static struct PyModuleDef moduledef = {
+        PyModuleDef_HEAD_INIT,
+        "_HelperSDT",
+        HelperSDT_doc,
+        -1,				/* m_size */
+        HelperSDT_methods,
+        NULL,				/* m_slots */
+        NULL,				/* m_traverse */
+        NULL,				/* m_clear */
+        NULL				/* m_free */
+};
+#endif
+
 
 PyMODINIT_FUNC
 init_HelperSDT(void)
 {
-    PyObject *m;
+    PyObject *module;
 
-    m = Py_InitModule3("_HelperSDT", HelperSDT_methods,
-		       HelperSDT_doc);
-    if (m == NULL)
+#if PY_MAJOR_VERSION >= 3
+    module = PyModule_Create(&moduledef);
+    if (module == NULL)
+	return NULL;
+    return module;
+#else
+    module = Py_InitModule3("_HelperSDT", HelperSDT_methods,
+			    HelperSDT_doc);
+    if (module == NULL)
 	return;
+#endif
 }
