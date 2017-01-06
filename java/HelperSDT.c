f8bc2a5eb3e91ce7d8d8af404b1f9890d6e2dac6
Frank Ch. Eigler <fche@redhat.com>
2017-01-06 17:37:37 -0500

    PR21020: reorganize data passing abi for java method parameters
    Under a new ABI, all java object types are supported for passing to
    java-method probes, by converting them all within the JVM to strings.
    The systemtap script side now gets $argX being all utf-char-pointers,
    which are loaded into script-side string variables with synthetic
    user_string_warn() function calls.
    
    A backward compatibility ABI is preserved in the java helper class
    as well as the translator, under --compatible=3.0.
    
    The java testsuite is drastically simplified (and execution time
    shortened) to one class.  These now test string and object passing
    too, with it and stapbm extended to be able to locate the HelperSDT
    jar / .so files within the systemtap installation $prefix.  This way,
    the system /usr/lib/jvm/.... path does not need to be populated just
    for stap tests to run.  (We should sometime extend the tests to cover
    all the argument arities, but eyeballing looks OK for now.)

--- java/HelperSDT.c
+++ java/HelperSDT.c
@@ -6,21 +6,28 @@
 #include "stdlib.h"
 #include "string.h"
 
-char* get_java_string(JNIEnv *env, jobject _string)
+
+/* java-stap ABI prior to stap version 3.1: pass some strings, some as cast int64_t values */
+
+
+/* Return strdup'd copy of incoming java string. 
+ * Might return NULL.
+ * Caller must free().
+ */
+static /*const*/ char* get_java_string(JNIEnv *env, jobject _string)
 {
   const char* __string = (*env)->GetStringUTFChars(env, _string, NULL);
-  (*env)->ReleaseStringUTFChars(env, _string, NULL);
-  if(__string != NULL){
-    char* string = strdup(__string);
-    return string;
-  }
-  else {
-    char* string = strdup("");
-    return string;
-  }
+  char *copied_string;
+  if(__string != NULL)
+    copied_string = strdup(__string);
+  else
+    copied_string = NULL;
+  /* NB: release the stringref only after strdup()ing it */
+  (*env)->ReleaseStringUTFChars(env, _string, __string);
+  return copied_string;
 }
 
-int64_t determine_java_type(JNIEnv *env, jobject _arg)
+static int64_t determine_java_type(JNIEnv *env, jobject _arg)
 {
   jclass class_arg = (*env)->GetObjectClass(env, _arg);
   jfieldID fidNumber = 0;
@@ -104,20 +111,10 @@ int64_t determine_java_type(JNIEnv *env, jobject _arg)
     {
       return (int64_t)(*env)->GetDoubleField(env, _arg, fidNumber);
     }
-  fidNumber = (*env)->GetFieldID(env, class_arg, "value", "Ljava/lang/String;");
-  if (NULL == fidNumber)
-    {
-      (*env)->ExceptionClear(env);
-      fidNumber = 0;
-    }
-  else
-    {
-      return (int64_t)(intptr_t)get_java_string(env, _arg);
-    }
-  jmethodID getMsgMeth = (*env)->GetMethodID(env, class_arg, "toString", "()Ljava/lang/String;");
-  jstring obj = (jstring)(*env)->CallObjectMethod(env, class_arg, getMsgMeth);
-  char* toString = get_java_string(env, obj);
-  return (int64_t)(intptr_t)toString;
+
+
+  /* Not a simple numeric scalar.  Map that to 0, else we get a memory leak with get_java_string() or worse. */
+  return (int64_t) 0;
 }
 /*
  * Class:     HelperSDT
@@ -315,6 +312,228 @@ JNIEXPORT void JNICALL Java_org_systemtap_byteman_helper_HelperSDT_METHOD_1STAP_
   STAP_PROBE11(HelperSDT, method__10, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, rulename);
   free(rulename);
 }
+
+
+
+/* java-stap ABI stap version 3.1: pass all parameters cast to strings */
+
+
+
+/* Return strdup'd copy of incoming java object's toString() value.
+ * Might return NULL.
+ * Caller must free().
+ */
+static /*const*/ char* get_java_tostring(JNIEnv *env, jobject _obj)
+{
+  jclass class_arg = (*env)->GetObjectClass(env, _obj);
+  jmethodID getMsgMeth = (*env)->GetMethodID(env, class_arg, "toString", "()Ljava/lang/String;");
+  jstring obj = (jstring)(*env)->CallObjectMethod(env, _obj, getMsgMeth);
+  /* (*env)->ExceptionClear(env); ? */
+  return get_java_string(env, obj);
+}
+
+
+JNIEXPORT void JNICALL Java_org_systemtap_byteman_helper_HelperSDT_METHOD_1STAP31_1PROBE0
+(JNIEnv *env, jobject obj, jstring _rulename)
+{
+  const char* rulename = (*env)->GetStringUTFChars(env, _rulename, NULL);
+  STAP_PROBE1(HelperSDT, method31__0, rulename);
+  (*env)->ReleaseStringUTFChars(env, _rulename, rulename);
+}
+
+JNIEXPORT void JNICALL Java_org_systemtap_byteman_helper_HelperSDT_METHOD_1STAP31_1PROBE1
+(JNIEnv *env, jobject obj, jstring _rulename, jobject _arg1)
+{
+  const char* rulename = (*env)->GetStringUTFChars(env, _rulename, NULL);
+  char* arg1 = get_java_tostring(env,_arg1);
+  STAP_PROBE2(HelperSDT, method31__1, arg1, rulename);
+  free(arg1);
+  (*env)->ReleaseStringUTFChars(env, _rulename, rulename);
+}
+
+JNIEXPORT void JNICALL Java_org_systemtap_byteman_helper_HelperSDT_METHOD_1STAP31_1PROBE2
+(JNIEnv *env, jobject obj, jstring _rulename, jobject _arg1, jobject _arg2)
+{
+  const char* rulename = (*env)->GetStringUTFChars(env, _rulename, NULL);
+  char* arg1 = get_java_tostring(env,_arg1);
+  char* arg2 = get_java_tostring(env,_arg2);
+  STAP_PROBE3(HelperSDT, method31__2, arg1, arg2, rulename);
+  free(arg2);
+  free(arg1);
+  (*env)->ReleaseStringUTFChars(env, _rulename, rulename);
+}
+
+JNIEXPORT void JNICALL Java_org_systemtap_byteman_helper_HelperSDT_METHOD_1STAP31_1PROBE3
+(JNIEnv *env, jobject obj, jstring _rulename, jobject _arg1, jobject _arg2, jobject _arg3)
+{
+  const char* rulename = (*env)->GetStringUTFChars(env, _rulename, NULL);
+  char* arg1 = get_java_tostring(env,_arg1);
+  char* arg2 = get_java_tostring(env,_arg2);
+  char* arg3 = get_java_tostring(env,_arg3);
+  STAP_PROBE4(HelperSDT, method31__3, arg1, arg2, arg3, rulename);
+  free(arg3);
+  free(arg2);
+  free(arg1);
+  (*env)->ReleaseStringUTFChars(env, _rulename, rulename);
+}
+
+JNIEXPORT void JNICALL Java_org_systemtap_byteman_helper_HelperSDT_METHOD_1STAP31_1PROBE4
+(JNIEnv *env, jobject obj, jstring _rulename, jobject _arg1, jobject _arg2, jobject _arg3, jobject _arg4)
+{
+  const char* rulename = (*env)->GetStringUTFChars(env, _rulename, NULL);
+  char* arg1 = get_java_tostring(env,_arg1);
+  char* arg2 = get_java_tostring(env,_arg2);
+  char* arg3 = get_java_tostring(env,_arg3);
+  char* arg4 = get_java_tostring(env,_arg4);
+  STAP_PROBE5(HelperSDT, method31__4, arg1, arg2, arg3, arg4, rulename);
+  free(arg4);
+  free(arg3);
+  free(arg2);
+  free(arg1);
+  (*env)->ReleaseStringUTFChars(env, _rulename, rulename);
+}
+
+JNIEXPORT void JNICALL Java_org_systemtap_byteman_helper_HelperSDT_METHOD_1STAP31_1PROBE5
+(JNIEnv *env, jobject obj, jstring _rulename, jobject _arg1, jobject _arg2, jobject _arg3, jobject _arg4, jobject _arg5)
+{
+  const char* rulename = (*env)->GetStringUTFChars(env, _rulename, NULL);
+  char* arg1 = get_java_tostring(env,_arg1);
+  char* arg2 = get_java_tostring(env,_arg2);
+  char* arg3 = get_java_tostring(env,_arg3);
+  char* arg4 = get_java_tostring(env,_arg4);
+  char* arg5 = get_java_tostring(env,_arg5);
+  STAP_PROBE6(HelperSDT, method31__5, arg1, arg2, arg3, arg4, arg5, rulename);
+  free(arg5);
+  free(arg4);
+  free(arg3);
+  free(arg2);
+  free(arg1);
+  (*env)->ReleaseStringUTFChars(env, _rulename, rulename);
+}
+
+JNIEXPORT void JNICALL Java_org_systemtap_byteman_helper_HelperSDT_METHOD_1STAP31_1PROBE6
+(JNIEnv *env, jobject obj, jstring _rulename, jobject _arg1, jobject _arg2, jobject _arg3, jobject _arg4, jobject _arg5, jobject _arg6)
+{
+  const char* rulename = (*env)->GetStringUTFChars(env, _rulename, NULL);
+  char* arg1 = get_java_tostring(env,_arg1);
+  char* arg2 = get_java_tostring(env,_arg2);
+  char* arg3 = get_java_tostring(env,_arg3);
+  char* arg4 = get_java_tostring(env,_arg4);
+  char* arg5 = get_java_tostring(env,_arg5);
+  char* arg6 = get_java_tostring(env,_arg6);
+  STAP_PROBE7(HelperSDT, method31__6, arg1, arg2, arg3, arg4, arg5, arg6, rulename);
+  free(arg6);
+  free(arg5);
+  free(arg4);
+  free(arg3);
+  free(arg2);
+  free(arg1);
+  (*env)->ReleaseStringUTFChars(env, _rulename, rulename);
+}
+
+JNIEXPORT void JNICALL Java_org_systemtap_byteman_helper_HelperSDT_METHOD_1STAP31_1PROBE7
+(JNIEnv *env, jobject obj, jstring _rulename, jobject _arg1, jobject _arg2, jobject _arg3, jobject _arg4, jobject _arg5, jobject _arg6, jobject _arg7)
+{
+  const char* rulename = (*env)->GetStringUTFChars(env, _rulename, NULL);
+  char* arg1 = get_java_tostring(env,_arg1);
+  char* arg2 = get_java_tostring(env,_arg2);
+  char* arg3 = get_java_tostring(env,_arg3);
+  char* arg4 = get_java_tostring(env,_arg4);
+  char* arg5 = get_java_tostring(env,_arg5);
+  char* arg6 = get_java_tostring(env,_arg6);
+  char* arg7 = get_java_tostring(env,_arg7);
+  STAP_PROBE8(HelperSDT, method31__7, arg1, arg2, arg3, arg4, arg5, arg6, arg7, rulename);
+  free(arg7);
+  free(arg6);
+  free(arg5);
+  free(arg4);
+  free(arg3);
+  free(arg2);
+  free(arg1);
+  (*env)->ReleaseStringUTFChars(env, _rulename, rulename);
+}
+
+JNIEXPORT void JNICALL Java_org_systemtap_byteman_helper_HelperSDT_METHOD_1STAP31_1PROBE8
+(JNIEnv *env, jobject obj, jstring _rulename, jobject _arg1, jobject _arg2, jobject _arg3, jobject _arg4, jobject _arg5, jobject _arg6, jobject _arg7, jobject _arg8)
+{
+  const char* rulename = (*env)->GetStringUTFChars(env, _rulename, NULL);
+  char* arg1 = get_java_tostring(env,_arg1);
+  char* arg2 = get_java_tostring(env,_arg2);
+  char* arg3 = get_java_tostring(env,_arg3);
+  char* arg4 = get_java_tostring(env,_arg4);
+  char* arg5 = get_java_tostring(env,_arg5);
+  char* arg6 = get_java_tostring(env,_arg6);
+  char* arg7 = get_java_tostring(env,_arg7);
+  char* arg8 = get_java_tostring(env,_arg8);
+  STAP_PROBE9(HelperSDT, method31__8, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, rulename);
+  free(arg8);
+  free(arg7);
+  free(arg6);
+  free(arg5);
+  free(arg4);
+  free(arg3);
+  free(arg2);
+  free(arg1);
+  (*env)->ReleaseStringUTFChars(env, _rulename, rulename);
+}
+
+JNIEXPORT void JNICALL Java_org_systemtap_byteman_helper_HelperSDT_METHOD_1STAP31_1PROBE9
+(JNIEnv *env, jobject obj, jstring _rulename, jobject _arg1, jobject _arg2, jobject _arg3, jobject _arg4, jobject _arg5, jobject _arg6, jobject _arg7, jobject _arg8, jobject _arg9)
+{
+  const char* rulename = (*env)->GetStringUTFChars(env, _rulename, NULL);
+  char* arg1 = get_java_tostring(env,_arg1);
+  char* arg2 = get_java_tostring(env,_arg2);
+  char* arg3 = get_java_tostring(env,_arg3);
+  char* arg4 = get_java_tostring(env,_arg4);
+  char* arg5 = get_java_tostring(env,_arg5);
+  char* arg6 = get_java_tostring(env,_arg6);
+  char* arg7 = get_java_tostring(env,_arg7);
+  char* arg8 = get_java_tostring(env,_arg8);
+  char* arg9 = get_java_tostring(env,_arg9);
+  STAP_PROBE10(HelperSDT, method31__9, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, rulename);
+  free(arg9);
+  free(arg8);
+  free(arg7);
+  free(arg6);
+  free(arg5);
+  free(arg4);
+  free(arg3);
+  free(arg2);
+  free(arg1);
+  (*env)->ReleaseStringUTFChars(env, _rulename, rulename);
+}
+
+JNIEXPORT void JNICALL Java_org_systemtap_byteman_helper_HelperSDT_METHOD_1STAP31_1PROBE10
+(JNIEnv *env, jobject obj, jstring _rulename, jobject _arg1, jobject _arg2, jobject _arg3, jobject _arg4, jobject _arg5, jobject _arg6, jobject _arg7, jobject _arg8, jobject _arg9, jobject _arg10)
+{
+  const char* rulename = (*env)->GetStringUTFChars(env, _rulename, NULL);
+  char* arg1 = get_java_tostring(env,_arg1);
+  char* arg2 = get_java_tostring(env,_arg2);
+  char* arg3 = get_java_tostring(env,_arg3);
+  char* arg4 = get_java_tostring(env,_arg4);
+  char* arg5 = get_java_tostring(env,_arg5);
+  char* arg6 = get_java_tostring(env,_arg6);
+  char* arg7 = get_java_tostring(env,_arg7);
+  char* arg8 = get_java_tostring(env,_arg8);
+  char* arg9 = get_java_tostring(env,_arg9);
+  char* arg10 = get_java_tostring(env,_arg10);
+  STAP_PROBE11(HelperSDT, method31__10, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, rulename);
+  free(arg10);
+  free(arg9);
+  free(arg8);
+  free(arg7);
+  free(arg6);
+  free(arg5);
+  free(arg4);
+  free(arg3);
+  free(arg2);
+  free(arg1);
+  (*env)->ReleaseStringUTFChars(env, _rulename, rulename);
+}
+
+
+
+/* java-stap ABI: backtrace passing */
 
 /*
  * Class:     HelperSDT
