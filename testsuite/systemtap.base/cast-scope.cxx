696dae5558a39dcb5a6af466fae07185916771e3
David Smith <dsmith@redhat.com>
2017-02-09 12:39:17 -0600

    Fix/update the cast-scope testcase for gcc 7.
    * testsuite/systemtap.base/cast-scope.exp: Update expected output for
      cast-scope.cxx change. This way we're depending on our own class instead
      of the internal details of the std::string class (which changed with gcc
      7).
    * testsuite/systemtap.base/cast-scope.cxx: Instead of using the
      std::string class, define our own test class.
    * testsuite/systemtap.base/cast-scope.stp: Update for cast-scope.cxx change.

--- testsuite/systemtap.base/cast-scope.cxx
+++ testsuite/systemtap.base/cast-scope.cxx
@@ -1,13 +1,38 @@
 #include "sys/sdt.h"
 
-#include <string>
+extern "C" {
+#include <string.h>
+}
+
+class sClass
+{
+private:
+    const char *_str;
+    size_t _l;
+
+public:
+    sClass(const char *n) : _str(n)
+    {
+	_l = strlen(_str);
+    }
+
+    const char *name(void)
+    {
+	return _str;
+    }
+
+    size_t len(void) const
+    {
+	return _l;
+    }
+};
 
 size_t
-length(const std::string& str)
+length(const sClass& str)
 {
     int res, r;
     STAP_PROBE1(cast-scope, length, &str);
-    r = str.length() * 2;
+    r = str.len() * 2;
     STAP_PROBE(cast-scope, dummy); /* Just here to probe line +5. */
     res = r / 2;
     STAP_PROBE(cast-scope, dummy2); /* Just here prevent line reordering. */
@@ -17,6 +42,6 @@ length(const std::string& str)
 int
 main()
 {
-    std::string hello = "Hello World!";
+    sClass hello = sClass("Hello World!");
     return 12 != length(hello);
 }
