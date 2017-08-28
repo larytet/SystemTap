larytet/SystemTap

commit 26382d613f4d266f74ddb3d3f3f36aceab171e14

Introduce runtime optimizations for statistical computations per PR10234.
This update improves the performance of systemtap runtime statistical
computations by optimizing out unneeded parts of the __stp_stat_add()
function.  It is based on parametrizing and inlining it.  The stap
translator now generates _stp_stat_add(), or _stp_pmap_add_*() calls
that have additional "optimization" parameters respective to stats
in use for given global.  GCC uses this for optimizing the inlined
__stp_stat_add() calls.

The optimization effect significantly depends on compiler version,
platform architecture, and the stat operators being used for given
global.  At the moment, the available stat operators are @count,
@sum, @min, @max, @avg, and @variance. The most computionally
expensive is @variance.  The effect of optimizing @variance is
significant.  Other stat operators are computionally chap and so
the effect of their optimizations is relatively low.

Using gcc-6.2.1-1.fc26.x86_64, the @count, @sum, @min, and @max
optimizations brings approximately 8% run time shrinkage. The
@variance optimization shrinkage is up to 70% using this compiler.
For other architectures, namely for power, the optimization is less
effective.

runtime/map-gen.c: Pass the additional optimization parameters through
	the map API generator macros.
runtime/map.c: Modify _new_map_set_stat() to accept optimization
        additional parameters.
runtime/map.h: Ditto.
runtime/pmap-gen.c: Pass the additional optimization parameters to
	__stp_map_set*() and to _stp_pmap_add*().
runtime/stat-common.c: Add optimization params to __stp_stat_add().
runtime/stat.c: Add optimization params to _stp_stat_add().
tapsets.cxx: Generate parametrized calls to the runtime.
translate.cxx: Ditto.
testsuite/systemtap.base/optim_stats*: New testcase.

runtime/stat-common.c
@@ -288,15 +288,13 @@ static void _stp_stat_print_histogram(Hist st, stat_data *sd)
 	_stp_print_flush();
 }
 
-static void __stp_stat_add(Hist st, stat_data *sd, int64_t val)
+static inline void __stp_stat_add(Hist st, stat_data *sd, int64_t val,
+                                  int stat_op_count, int stat_op_sum, int stat_op_min,
+				  int stat_op_max, int stat_op_variance)
 {
 	int n;
 	int delta = 0;
 
-	/*
-	 * Below, we use Welford's online algorithm for computing variance.
-	 * https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance
-	 */
 	sd->shift = st->bit_shift;
 	sd->stat_ops = st->stat_ops;
 	if (sd->count == 0) {
@@ -305,20 +303,19 @@ static void __stp_stat_add(Hist st, stat_data *sd, int64_t val)
 		sd->avg_s = val << sd->shift;
 		sd->_M2 = 0;
 	} else {
-		sd->count++;
-		sd->sum += val;
-		if (val > sd->max)
+		if(stat_op_count)
+			sd->count++;
+		if(stat_op_sum)
+			sd->sum += val;
+		if (stat_op_min && (val > sd->max))
 			sd->max = val;
-		if (val < sd->min)
+		if (stat_op_max && (val < sd->min))
 			sd->min = val;
 		/*
-		 * Following is an optimization that improves performance
-		 * in case @variance() isn't used with given global.
-		 *
-		 * Note that this doesn't affect computing of @avg(), which
-		 * happens within the per-CPU aggregation functions.
+		 * Below, we use Welford's online algorithm for computing variance.
+		 * https://en.wikipedia.org/wiki/Algorithms_for_calculating_variance
 		 */
-		if (sd->stat_ops & STAT_OP_VARIANCE) {
+		if (stat_op_variance) {
 		    delta = (val << sd->shift) - sd->avg_s;
 		    sd->avg_s += _stp_div64(NULL, delta, sd->count);
 		    sd->_M2 += delta * ((val << sd->shift) - sd->avg_s);

runtime/map-gen.c
@@ -48,8 +48,8 @@
 #define VALN s
 #define VALSTOR char value[MAP_STRING_LENGTH]
 #define MAP_GET_VAL(node) ((node)->value)
-#define MAP_SET_VAL(map,node,val,add) _new_map_set_str(map,MAP_GET_VAL(node),val,add)
-#define MAP_COPY_VAL(map,node,val,add) MAP_SET_VAL(map,node,val,add)
+#define MAP_SET_VAL(map,node,val,add,s1,s2,s3,s4,s5) _new_map_set_str(map,MAP_GET_VAL(node),val,add)
+#define MAP_COPY_VAL(map,node,val,add) MAP_SET_VAL(map,node,val,add,0,0,0,0,0)
 #define NULLRET ""
 #elif VALUE_TYPE == INT64
 #define VALTYPE int64_t
@@ -58,8 +58,8 @@
 #define VALN i
 #define VALSTOR int64_t value
 #define MAP_GET_VAL(node) ((node)->value)
-#define MAP_SET_VAL(map,node,val,add) _new_map_set_int64(map,&MAP_GET_VAL(node),val,add)
-#define MAP_COPY_VAL(map,node,val,add) MAP_SET_VAL(map,node,val,add)
+#define MAP_SET_VAL(map,node,val,add,s1,s2,s3,s4,s5) _new_map_set_int64(map,&MAP_GET_VAL(node),val,add)
+#define MAP_COPY_VAL(map,node,val,add) MAP_SET_VAL(map,node,val,add,0,0,0,0,0)
 #define NULLRET (int64_t)0
 #elif VALUE_TYPE == STAT
 #define VALTYPE stat_data*
@@ -68,7 +68,7 @@
 #define VALN x
 #define VALSTOR stat_data value
 #define MAP_GET_VAL(node) (&(node)->value)
-#define MAP_SET_VAL(map,node,val,add) _new_map_set_stat(map,MAP_GET_VAL(node),val,add)
+#define MAP_SET_VAL(map,node,val,add,s1,s2,s3,s4,s5) _new_map_set_stat(map,MAP_GET_VAL(node),val,add,s1,s2,s3,s4,s5)
 #define MAP_COPY_VAL(map,node,val,add) _new_map_copy_stat(map,MAP_GET_VAL(node),val,add)
 #define NULLRET (stat_data*)0
 #else
@@ -799,7 +799,7 @@ static MAP KEYSYM(_stp_map_new) (int first_arg, ...)
 
 #endif /* VALUE_TYPE */
 
-static int KEYSYM(__stp_map_set) (MAP map, ALLKEYSD(key), VSTYPE val, int add)
+static inline int KEYSYM(__stp_map_set) (MAP map, ALLKEYSD(key), VSTYPE val, int add, int s1, int s2, int s3, int s4, int s5)
 {
 	unsigned int hv;
 	struct mhlist_head *head;
@@ -817,7 +817,7 @@ static int KEYSYM(__stp_map_set) (MAP map, ALLKEYSD(key), VSTYPE val, int add)
 
 	mhlist_for_each_entry(n, e, head, node.hnode) {
 		if (KEY_EQ_P(n)) {
-			return MAP_SET_VAL(map, n, val, add);
+			return MAP_SET_VAL(map, n, val, add, s1, s2, s3, s4, s5);
 		}
 	}
 	/* key not found */
@@ -825,17 +825,17 @@ static int KEYSYM(__stp_map_set) (MAP map, ALLKEYSD(key), VSTYPE val, int add)
 	if (n == NULL)
 		return -1;
 	KEYCPY(n);
-	return MAP_SET_VAL(map, n, val, 0);
+	return MAP_SET_VAL(map, n, val, 0, s1, s2, s3, s4, s5);
 }
 
 static int KEYSYM(_stp_map_set) (MAP map, ALLKEYSD(key), VSTYPE val)
 {
-	return KEYSYM(__stp_map_set) (map, ALLKEYS(key), val, 0);
+	return KEYSYM(__stp_map_set) (map, ALLKEYS(key), val, 0, 1, 1, 1, 1, 1);
 }
 
 static int KEYSYM(_stp_map_add) (MAP map, ALLKEYSD(key), VSTYPE val)
 {
-	return KEYSYM(__stp_map_set) (map, ALLKEYS(key), val, 1);
+	return KEYSYM(__stp_map_set) (map, ALLKEYS(key), val, 1, 1, 1, 1, 1, 1);
 }
 
 

runtime/map.c
@@ -436,7 +436,7 @@ static int _new_map_set_str (MAP map, char *dst, char *val, int add)
 	return 0;
 }
 
-static int _new_map_set_stat (MAP map, struct stat_data *sd, int64_t val, int add)
+static int _new_map_set_stat (MAP map, struct stat_data *sd, int64_t val, int add, int s1, int s2, int s3, int s4, int s5)
 {
 	if (!add) {
 		Hist st = &map->hist;
@@ -449,7 +449,7 @@ static int _new_map_set_stat (MAP map, struct stat_data *sd, int64_t val, int ad
 	}
 	(&map->hist)->bit_shift = map->bit_shift;
 	(&map->hist)->stat_ops = map->stat_ops;
-	__stp_stat_add (&map->hist, sd, val);
+	__stp_stat_add (&map->hist, sd, val, s1, s2, s3, s4, s5);
 	return 0;
 }
 

runtime/map.h
@@ -179,7 +179,7 @@ static void _stp_pmap_del(PMAP pmap);
 static MAP _stp_pmap_agg (PMAP pmap, map_update_fn update, map_cmp_fn cmp);
 static struct map_node *_stp_new_agg(MAP agg, struct mhlist_head *ahead,
 				     struct map_node *ptr, map_update_fn update);
-static int _new_map_set_stat (MAP map, struct stat_data *dst, int64_t val, int add);
+static int _new_map_set_stat (MAP map, struct stat_data *dst, int64_t val, int add, int s1, int s2, int s3, int s4, int s5);
 static int _new_map_copy_stat (MAP map, struct stat_data *dst, struct stat_data *src, int add);
 static void _stp_map_sort (MAP map, int keynum, int dir, map_get_key_fn get_key);
 static void _stp_map_sortn(MAP map, int n, int keynum, int dir, map_get_key_fn get_key);

runtime/pmap-gen.c
@@ -234,18 +234,18 @@ static int KEYSYM(_stp_pmap_set) (PMAP pmap, ALLKEYSD(key), VSTYPE val)
 {
 	int res;
 	MAP m = _stp_pmap_get_map (pmap, MAP_GET_CPU());
-	res = KEYSYM(__stp_map_set) (m, ALLKEYS(key), val, 0);
+	res = KEYSYM(__stp_map_set) (m, ALLKEYS(key), val, 0, 1, 1, 1, 1, 1);
         MAP_PUT_CPU();
 	return res;
 }
 
-static int KEYSYM(_stp_pmap_add) (PMAP pmap, ALLKEYSD(key), VSTYPE val)
+static inline int KEYSYM(_stp_pmap_add) (PMAP pmap, ALLKEYSD(key), VSTYPE val, int s1, int s2, int s3, int s4, int s5)
 {
 	int res;
 	MAP m = _stp_pmap_get_map (pmap, MAP_GET_CPU());
 	m->bit_shift = pmap->bit_shift;
 	m->stat_ops = pmap->stat_ops;
-	res = KEYSYM(__stp_map_set) (m, ALLKEYS(key), val, 1);
+	res = KEYSYM(__stp_map_set) (m, ALLKEYS(key), val, 1, s1, s2, s3, s4, s5);
         MAP_PUT_CPU();
 	return res;
 }

runtime/stat.c
@@ -130,20 +130,31 @@ static void _stp_stat_del (Stat st)
 }
 
 /** Add to a Stat.
- * Add an int64 to a Stat.
+ * Add an int64 to a Stat, and for optimization purposes specify which
+ * statistical operators are bound to given Stat.  Set all of stat_op*
+ * to 1 if unsure.  Note that @avg() is being evaluated separately based
+ * on @sum and @count within the code directly generated by the translator.
  *
  * @param st Stat
  * @param val Value to add
+ * @param stat_op_count int
+ * @param stat_op_sum int
+ * @param stat_op_min int
+ * @param stat_op_max int
+ * @param stat_op_variance int
+ *
  */
-static void _stp_stat_add (Stat st, int64_t val)
+static inline void _stp_stat_add (Stat st, int64_t val, int stat_op_count,
+                                  int stat_op_sum, int stat_op_min,
+				  int stat_op_max, int stat_op_variance)
 {
 	stat_data *sd = _stp_stat_per_cpu_ptr (st, STAT_GET_CPU());
 	STAT_LOCK(sd);
-	__stp_stat_add (&st->hist, sd, val);
+	__stp_stat_add (&st->hist, sd, val, stat_op_count, stat_op_sum,
+	                stat_op_min, stat_op_max, stat_op_variance);
 	STAT_UNLOCK(sd);
 	STAT_PUT_CPU();
 }
-
 
 static void _stp_stat_clear_data (Stat st, stat_data *sd)
 {

tapsets.cxx
@@ -269,7 +269,8 @@ common_probe_entryfn_epilogue (systemtap_session& s,
     }
 
   s.op->newline() << "#ifdef STP_TIMING";
-  s.op->newline() << "if (likely (stat)) _stp_stat_add(stat, cycles_elapsed);";
+  // STP_TIMING requires min, max, avg (and thus count and sum), but not variance.
+  s.op->newline() << "if (likely (stat)) _stp_stat_add(stat, cycles_elapsed, 1, 1, 1, 1, 0);";
   s.op->newline() << "#endif";
 
   if (overload_processing && !s.runtime_usermode_p())

testsuite/systemtap.base/optim_stats.exp
@@ -1,0 +1,31 @@
+# This is a test for stat run time optimizations.
+# See corresponding .stp file for details.
+
+set test "optim_stats"
+
+if {![installtest_p]} {
+    untested $test
+    return
+}
+
+for {set i 1} {$i <= 2} {incr i} {
+    foreach runtime [get_runtime_list] {
+	if {$runtime != ""} {
+	    spawn stap --runtime=$runtime -g --suppress-time-limits $srcdir/$subdir/$test$i.stp
+	} else {
+	    spawn stap -g --suppress-time-limits $srcdir/$subdir/$test$i.stp
+	}
+
+	expect {
+	    -timeout 300
+	    -re {^IGNORE[^\r\n]+\r\n} { exp_continue }
+	    -re {^PASS test1[^\r\n]+\r\n} { pass "$test$i.stp subtest1 $runtime"; exp_continue }
+	    -re {^PASS test2[^\r\n]+\r\n} { pass "$test$i.stp subtest2 $runtime"; exp_continue }
+	    -re {^FAIL test1[^\r\n]+\r\n} { fail "$test$i.stp subtest1 $runtime"; exp_continue }
+	    -re {^FAIL test2[^\r\n]+\r\n} { fail "$test$i.stp subtest2 $runtime"; exp_continue }
+	    timeout {fail "$test: unexpected timeout"}
+	    eof { }
+	}
+	catch {close}; catch {wait}
+    }
+}

commit c8c24917452665d3ea42019f8700c9164bf3091a

PR22005: Fix @min() and @max() extractor functions.
Commit 26382d613f4d26 introduced a regression in @min()
and @max() extractor functions.  Example:

$ stap -e 'global n probe oneshot {n<<<12 n<<<34 println(@max(n))}'
12
$

This commit fixes the regression and adds a testcase.

runtime/stat-common.c
@@ -307,9 +307,9 @@ static inline void __stp_stat_add(Hist st, stat_data *sd, int64_t val,
 			sd->count++;
 		if(stat_op_sum)
 			sd->sum += val;
-		if (stat_op_min && (val > sd->max))
+		if (stat_op_max && (val > sd->max))
 			sd->max = val;
-		if (stat_op_max && (val < sd->min))
+		if (stat_op_min && (val < sd->min))
 			sd->min = val;
 		/*
 		 * Below, we use Welford's online algorithm for computing variance.

testsuite/systemtap.base/pr22005.exp
@@ -1,0 +1,7 @@
+set test_name "pr22005"
+
+set ::result_string "34"
+stap_run2 -e "global l probe oneshot \{l<<<12 l<<<34 println(@max(l))\}"
+
+set ::result_string "21"
+stap_run2 -e "global l probe oneshot \{l<<<43 l<<<21 println(@min(l))\}"