larytet/SystemTap

commit 586c8666a96f9445816eafba45067263b6d199b4

Add more http client updates and rearrange some code.
* client-nss.cxx: Moved MOK_PUBLIC_CERT_NAME to cscommon.h.
  (nss_client_backend:nss_client_backend): Moved here from client-nss.h.
  (process_response): Moved to csclient.cxx.
* client-nss.h (nss_client_backend): Moved class constructor to
  client-nss.cxx.
* client-http.h (http_client_backend): Moved class constructor to
  client-http.cxx.
* client-http.cxx (http_client_backend::http_client_backend): Moved here
  from header file and initialize 'server_tmpdir'.
  (http_client_backend::unpack_response): Move processing from
  process_response() to here and delete process_response().
* csclient.cxx (process_response): Moved from client-nss.cxx.
* csclient.h: Move 'process_response' from the backend to the main
  code. Add 'server_tmpdir' and 'server_version' to the backend.
* cscommon.h: Move MOK_PUBLIC_CERT_NAME and MOK_PUBLIC_CERT_FILE
  definitions here.
* stap-serverd.cxx: Remove MOK_PUBLIC_CERT_NAME and MOK_PUBLIC_CERT_FILE
  definitions.
* util.cxx: Add another write_to_file() template instantiation.

csclient.cxx
@@ -24,6 +24,7 @@
 extern "C" {
 #include <sys/times.h>
 #include <sys/time.h>
+#include <glob.h>
 }
 
 using namespace std;
@@ -77,7 +78,7 @@ compile_server_client::passes_0_4 ()
   rc = backend->unpack_response ();
   assert_no_interrupts();
   if (rc != 0) goto done;
-  rc = backend->process_response ();
+  rc = process_response ();
 
  done:
   struct tms tms_after;
@@ -258,6 +259,91 @@ compile_server_client::create_request ()
       if (rc != 0)
 	return rc;
     }
+
+  return rc;
+}
+
+int
+compile_server_client::process_response ()
+{
+  // Pick up the results of running stap on the server.
+  string filename = backend->server_tmpdir + "/rc";
+  int stap_rc;
+  int rc = read_from_file (filename, stap_rc);
+  if (rc != 0)
+    return rc;
+  rc = stap_rc;
+
+  if (s.last_pass >= 4)
+    {
+      // The server should have returned a module.
+      string filespec = s.tmpdir + "/*.ko";
+      if (s.verbose >= 3)
+       clog << _F("Searching \"%s\"\n", filespec.c_str());
+
+      glob_t globbuf;
+      int r = glob(filespec.c_str (), 0, NULL, & globbuf);
+      if (r != GLOB_NOSPACE && r != GLOB_ABORTED && r != GLOB_NOMATCH)
+	{
+	  if (globbuf.gl_pathc > 1)
+	    clog << _("Incorrect number of modules in server response") << endl;
+	  else
+	    {
+	      assert (globbuf.gl_pathc == 1);
+	      string modname = globbuf.gl_pathv[0];
+	      if (s.verbose >= 3)
+		clog << _("  found ") << modname << endl;
+
+	      // If a module name was not specified by the user, then set it to
+	      // be the one generated by the server.
+	      if (! s.save_module)
+		{
+		  vector<string> components;
+		  tokenize (modname, components, "/");
+		  s.module_name = components.back ();
+		  s.module_name.erase(s.module_name.size() - 3);
+		}
+
+	      // If a uprobes.ko module was returned, then make note of it.
+	      string uprobes_ko;
+	      if (backend->server_version < "1.6")
+		uprobes_ko = s.tmpdir + "/server/uprobes.ko";
+	      else
+		uprobes_ko = s.tmpdir + "/uprobes/uprobes.ko";
+
+	      if (file_exists (uprobes_ko))
+		{
+		  s.need_uprobes = true;
+		  s.uprobes_path = uprobes_ko;
+		}
+	    }
+	}
+      else if (s.have_script)
+	{
+	  if (rc == 0)
+	    {
+	      clog << _("No module was returned by the server.") << endl;
+	      rc = 1;
+	    }
+	}
+      globfree (& globbuf);
+    }
+
+  // If the server returned a MOK certificate, copy it to the user's
+  // current directory.
+  string server_MOK_public_cert = backend->server_tmpdir + "/" MOK_PUBLIC_CERT_NAME;
+  if (file_exists (server_MOK_public_cert))
+    {
+      string dst = MOK_PUBLIC_CERT_NAME;
+      copy_file (server_MOK_public_cert, dst, (s.verbose >= 3));
+    }
+
+  // Output stdout and stderr.
+  filename = backend->server_tmpdir + "/stderr";
+  flush_to_stream (filename, clog);
+
+  filename = backend->server_tmpdir + "/stdout";
+  flush_to_stream (filename, cout);
 
   return rc;
 }

client-http.cxx
@@ -187,18 +187,14 @@ http_client::download (const std::string & url, http_client::download_type type)
   else if (type == file_type)
     {
       std::string filename = url;
-      std::string ko_suffix = ".ko\"";
       std::string filepath;
+
       if (filename.back() == '/')
         filename.erase(filename.length()-1);
-
-      if (std::equal(ko_suffix.rbegin(), ko_suffix.rend(), filename.rbegin()))
-        filepath = s.tmpdir + "/" + s.module_name + ".ko";
-      else
-        filepath = s.tmpdir + "/" + filename.substr (filename.rfind ('/')+1);
+      filepath = s.tmpdir + "/" + filename.substr (filename.rfind ('/')+1);
 
       if (s.verbose >= 3)
-        clog << "Downloaded " + filepath << endl;
+	clog << "Downloaded " + filepath << endl;
       std::FILE *File = std::fopen (filepath.c_str(), "wb");
       curl_easy_setopt (curl, CURLOPT_WRITEDATA, File);
       curl_easy_setopt (curl, CURLOPT_WRITEFUNCTION, http_client::get_file);
@@ -655,9 +651,15 @@ http_client::add_script_file (std::string script_type, std::string script_file)
 void
 http_client::add_module (std::string module)
 {
-    modules.push_back (module);
+  modules.push_back (module);
 }
 
+
+http_client_backend::http_client_backend (systemtap_session &s)
+  : client_backend(s)
+{
+  server_tmpdir = s.tmpdir;
+}
 
 int
 http_client_backend::initialize ()
@@ -715,13 +717,6 @@ http_client_backend::find_and_connect_to_server ()
 int
 http_client_backend::unpack_response ()
 {
-  return 0;
-}
-
-int
-http_client_backend::process_response ()
-{
-
   std::string::size_type found = http->host.find ("/builds");
   std::string uri;
   std::map<std::string, std::string>::iterator it_loc;
@@ -776,6 +771,20 @@ http_client_backend::process_response ()
       return 1;
     }
 
+  // Get the return code information.
+  json_object *rc_obj;
+  json_bool jfound = json_object_object_get_ex (http->root, "rc", &rc_obj);
+  if (jfound)
+    {
+      int rc = json_object_get_int(rc_obj);
+      write_to_file(s.tmpdir + "/rc", rc);
+    }
+  else
+    {
+      clog << "Couldn't find 'rc' in JSON results data" << endl;
+      return 1;
+    }
+
   // Download each item in the optional 'files' array. This is
   // optional since not all stap invocations produce an output file
   // (like a module).
@@ -787,52 +796,40 @@ http_client_backend::process_response ()
         {
 	  json_object *files_element = json_object_array_get_idx (files, k);
 	  json_object *loc;
-	  found = json_object_object_get_ex (files_element, "location", &loc);
+	  jfound = json_object_object_get_ex (files_element, "location", &loc);
 	  string location = json_object_get_string (loc);
 	  http->download (http->host + location, http->file_type);
 	}
     }
 
-  // Download the stdout and stderr file items.
-  json_object *stdio_loc;
-  found = json_object_object_get_ex (http->root, "stderr_location",
-				     &stdio_loc);
-  if (found)
+  // Output stdout and stderr.
+  json_object *loc_obj;
+  jfound = json_object_object_get_ex (http->root, "stderr_location", &loc_obj);
+  if (jfound)
     {
-      string stdio_loc_str = json_object_get_string (stdio_loc);
-      http->download (http->host + stdio_loc_str, http->file_type);
-
-      std::ifstream ferr (s.tmpdir + "/stderr");
-      if (ferr.is_open())
-	std::cout << ferr.rdbuf() << endl;
-      ferr.close();
+      string loc_str = json_object_get_string (loc_obj);
+      http->download (http->host + loc_str, http->file_type);
     }
   else
     {
-      clog << "Couldn't find 'stderr' in JSON data" << endl;
+      clog << "Couldn't find 'stderr' in JSON results data" << endl;
       return 1;
     }
 
-  found = json_object_object_get_ex (http->root, "stdout_location",
-				     &stdio_loc);
-  if (found)
+  jfound = json_object_object_get_ex (http->root, "stdout_location", &loc_obj);
+  if (jfound)
     {
-      string stdio_loc_str = json_object_get_string (stdio_loc);
-      http->download (http->host + stdio_loc_str, http->file_type);
-
-      std::ifstream fout (s.tmpdir + "/stdout");
-      if (fout.is_open())
-	std::cout << fout.rdbuf() << endl;
-      fout.close();
+      string loc_str = json_object_get_string (loc_obj);
+      http->download (http->host + loc_str, http->file_type);
     }
   else
     {
-      clog << "Couldn't find 'stdout' in JSON data" << endl;
+      clog << "Couldn't find 'stdout' in JSON results data" << endl;
       return 1;
     }
-
   return 0;
 }
+
 
 int
 http_client_backend::add_protocol_version (const std::string &version)

client-http.h
@@ -19,7 +19,7 @@ class http_client;
 class http_client_backend : public client_backend
 {
 public:
-  http_client_backend (systemtap_session &s) : client_backend(s) {}
+  http_client_backend (systemtap_session &s);
 
   friend class http_client;
 

client-nss.cxx
@@ -74,8 +74,6 @@ using namespace std;
 #define STAP_CSC_04 _("Unable to open output file %s\n")
 #define STAP_CSC_05 _("could not write to %s\n")
 
-#define MOK_PUBLIC_CERT_NAME "signing_key.x509"
-
 static PRIPv6Addr &copyAddress (PRIPv6Addr &PRin6, const in6_addr &in6);
 static PRNetAddr &copyNetAddr (PRNetAddr &x, const PRNetAddr &y);
 bool operator!= (const PRNetAddr &x, const PRNetAddr &y);
@@ -812,6 +810,11 @@ client_connect (const compile_server_info &server,
   return errCode;
 }
 
+nss_client_backend::nss_client_backend (systemtap_session &s)
+  : client_backend(s), argc(0)
+{
+  server_tmpdir = s.tmpdir + "/server";
+}
 
 int
 nss_client_backend::initialize ()
@@ -1216,7 +1219,6 @@ int
 nss_client_backend::unpack_response ()
 {
   // Unzip the response package.
-  server_tmpdir = s.tmpdir + "/server";
   vector<string> cmd { "unzip", "-qd", server_tmpdir, server_zipfile };
   int rc = stap_system (s.verbose, cmd);
   if (rc != 0)
@@ -1310,91 +1312,6 @@ nss_client_backend::show_server_compatibility () const
       clog << _F("Server protocol version is %s\n", server_version.v);
       clog << _("The server does not use localization information passed by the client\n");
     }
-}
-
-int
-nss_client_backend::process_response ()
-{
-  // Pick up the results of running stap on the server.
-  string filename = server_tmpdir + "/rc";
-  int stap_rc;
-  int rc = read_from_file (filename, stap_rc);
-  if (rc != 0)
-    return rc;
-  rc = stap_rc;
-
-  if (s.last_pass >= 4)
-    {
-      // The server should have returned a module.
-      string filespec = s.tmpdir + "/*.ko";
-      if (s.verbose >= 3)
-       clog << _F("Searching \"%s\"\n", filespec.c_str());
-
-      glob_t globbuf;
-      int r = glob(filespec.c_str (), 0, NULL, & globbuf);
-      if (r != GLOB_NOSPACE && r != GLOB_ABORTED && r != GLOB_NOMATCH)
-	{
-	  if (globbuf.gl_pathc > 1)
-	    clog << _("Incorrect number of modules in server response") << endl;
-	  else
-	    {
-	      assert (globbuf.gl_pathc == 1);
-	      string modname = globbuf.gl_pathv[0];
-	      if (s.verbose >= 3)
-		clog << _("  found ") << modname << endl;
-
-	      // If a module name was not specified by the user, then set it to
-	      // be the one generated by the server.
-	      if (! s.save_module)
-		{
-		  vector<string> components;
-		  tokenize (modname, components, "/");
-		  s.module_name = components.back ();
-		  s.module_name.erase(s.module_name.size() - 3);
-		}
-
-	      // If a uprobes.ko module was returned, then make note of it.
-	      string uprobes_ko;
-	      if (server_version < "1.6")
-		uprobes_ko = s.tmpdir + "/server/uprobes.ko";
-	      else
-		uprobes_ko = s.tmpdir + "/uprobes/uprobes.ko";
-
-	      if (file_exists (uprobes_ko))
-		{
-		  s.need_uprobes = true;
-		  s.uprobes_path = uprobes_ko;
-		}
-	    }
-	}
-      else if (s.have_script)
-	{
-	  if (rc == 0)
-	    {
-	      clog << _("No module was returned by the server.") << endl;
-	      rc = 1;
-	    }
-	}
-      globfree (& globbuf);
-    }
-
-  // If the server returned a MOK certificate, copy it to the user's
-  // current directory.
-  string server_MOK_public_cert = s.tmpdir + "/server/" MOK_PUBLIC_CERT_NAME;
-  if (file_exists (server_MOK_public_cert))
-    {
-      string dst = MOK_PUBLIC_CERT_NAME;
-      copy_file (server_MOK_public_cert, dst, (s.verbose >= 3));
-    }
-
-  // Output stdout and stderr.
-  filename = server_tmpdir + "/stderr";
-  flush_to_stream (filename, clog);
-
-  filename = server_tmpdir + "/stdout";
-  flush_to_stream (filename, cout);
-
-  return rc;
 }
 
 // Issue a status message for when a server's trust is already in place.

client-nss.h
@@ -26,7 +26,7 @@ struct compile_server_info;
 class nss_client_backend : public client_backend
 {
 public:
-  nss_client_backend (systemtap_session &s) : client_backend(s), argc(0) {}
+  nss_client_backend (systemtap_session &s);
 
   int initialize ();
   int add_protocol_version (const std::string &version);
@@ -46,14 +46,12 @@ public:
   int package_request ();
   int find_and_connect_to_server ();
   int unpack_response ();
-  int process_response ();
 
 private:
   unsigned argc;
   std::string client_zipfile;
   std::string server_tmpdir;
   std::string server_zipfile;
-  cs_protocol_version server_version;
   std::string locale_vars;
   std::ostringstream mok_fingerprints;
 

csclient.h
@@ -22,7 +22,6 @@ public:
   virtual int package_request () = 0;
   virtual int find_and_connect_to_server () = 0;
   virtual int unpack_response () = 0;
-  virtual int process_response () = 0;
 
   virtual int add_protocol_version (const std::string &version) = 0;
   virtual int add_sysinfo () = 0;
@@ -37,6 +36,9 @@ public:
 
   virtual void add_mok_fingerprint(const std::string &fingerprint) = 0;
   virtual int finalize_mok_fingerprints() = 0;
+
+  std::string server_tmpdir;
+  cs_protocol_version server_version;
 
 protected:
   systemtap_session &s;
@@ -53,6 +55,7 @@ private:
   // Client/server session methods.
   int initialize ();
   int create_request ();
+  int process_response ();
 
   // Client/server utility methods.
   int add_cmd_args ();

cscommon.h
@@ -2,6 +2,9 @@
 #ifndef CSCOMMON_H
 #define CSCOMMON_H 1
 
+#define MOK_PUBLIC_CERT_NAME "signing_key.x509"
+#define MOK_PUBLIC_CERT_FILE "/" MOK_PUBLIC_CERT_NAME
+
 #if HAVE_NSS
 extern "C"
 {

stap-serverd.cxx
@@ -73,8 +73,6 @@ static PRStatus spawn_and_wait (const vector<string> &argv, int *result,
                                 const char* fd0, const char* fd1, const char* fd2,
 				const char *pwd, const vector<string>& envVec = vector<string> ());
 
-#define MOK_PUBLIC_CERT_NAME "signing_key.x509"
-#define MOK_PUBLIC_CERT_FILE "/" MOK_PUBLIC_CERT_NAME
 #define MOK_PRIVATE_CERT_NAME "signing_key.priv"
 #define MOK_PRIVATE_CERT_FILE "/" MOK_PRIVATE_CERT_NAME
 #define MOK_CONFIG_FILE "/x509.genkey"

commit 50436433162be8beefb179700725ea3a2c227179

Fix PR22278 by fixing server breakage caused by commit 586c8666a.
* csclient.cxx (create_request): Let the backend add the argument in
  include_file_or_directory().
* testsuite/systemtap.server/server.exp: Add 3 simple tests: a filename, a
  "-L", and a "-I tapset -e SCRIPT".
* testsuite/lib/systemtap.exp (stap_run_batch): If the "filename" argument
  is empty, assume this stap command line doesn't need a file.

csclient.cxx
@@ -221,14 +221,11 @@ compile_server_client::create_request ()
       unsigned limit = s.include_path.size ();
       for (unsigned i = s.include_arg_start; i < limit; ++i)
 	{
-	  rc = backend->include_file_or_directory ("tapset",
-						   s.include_path[i]);
-	  if (rc != 0)
-	    return rc;
 	  rc = backend->add_cmd_arg ("-I");
 	  if (rc != 0)
 	    return rc;
-	  rc = backend->add_cmd_arg ("tapset");
+	  rc = backend->include_file_or_directory ("tapset",
+						   s.include_path[i]);
 	  if (rc != 0)
 	    return rc;
 	}

testsuite/lib/systemtap.exp
@@ -682,36 +682,38 @@ proc systemtap_finish {args} {
 proc systemtap_version {} {}
 
 proc stap_run_batch {filename args} {
-    verbose -log "starting $filename $args"
+    if {[string length $filename]} {
+	# Many of our test cases use "#! stap ...".  Since these lack
+	# /full/paths, they are not really executable.  (We can't have
+	# /full/paths because the choice of systemtap interpreter is set
+	# at "make check" time.)
 
-    # Many of our test cases use "#! stap ...".  Since these lack
-    # /full/paths, they are not really executable.  (We can't have
-    # /full/paths because the choice of systemtap interpreter is set
-    # at "make check" time.)
-
-    # So we cheat.  If the file begins with "#! stap", we will spawn
-    # stap manually here (relying on $PATH).  Otherwise, we presume
-    # the file properly executable.
-
-    set file [open $filename r]
-    set firstbits [gets $file]
-    close $file
-    if [regexp -line {\#! stap (.*)} $firstbits -> stap_args] {
-        verbose -log "spawn1 stap $stap_args $filename $args"
-        # Make sure we don't accidentially add an extra empty argument.
-        if {$args == ""} {
-            eval spawn stap $stap_args $filename
-        } else {
-            eval spawn stap $stap_args $filename $args
-        }
+	# So we cheat.  If the file begins with "#! stap", we will spawn
+	# stap manually here (relying on $PATH).  Otherwise, we presume
+	# the file properly executable.
+	set file [open $filename r]
+	set firstbits [gets $file]
+	close $file
+	if {[regexp -line {\#! stap (.*)} $firstbits -> stap_args]} {
+	    verbose -log "spawn1 stap $stap_args $filename $args"
+	    # Make sure we don't accidentially add an extra empty argument.
+	    if {$args == ""} {
+		eval spawn stap $stap_args $filename
+	    } else {
+		eval spawn stap $stap_args $filename $args
+	    }
+	} else {
+	    verbose -log "spawn2 $filename $args"
+	    # Make sure we don't accidentially add an extra empty argument.
+	    if {$args == ""} {
+		spawn $filename
+	    } else {
+		spawn $filename $args
+	    }
+	}
     } else {
-        verbose -log "spawn2 $filename $args"
-        # Make sure we don't accidentially add an extra empty argument.
-        if {$args == ""} {
-            spawn $filename
-        } else {
-            spawn $filename $args
-        }
+	verbose -log "spawn3 stap $args"
+	eval spawn stap $args
     }
 
     expect {

testsuite/systemtap.server/tapset/foo.stp
@@ -1,0 +1,4 @@
+function foo()
+{
+    printf("Hello\n")
+}